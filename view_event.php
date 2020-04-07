9d912cf79dbff56840efa0d13dd7d2c2fe6fa70c
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-04-07 12:27:38 +0530

    Add files via upload

--- view_event.php
+++ view_event.php
@@ -28,6 +28,12 @@
             <div id=title_box>
                 <?php echo $result['name'] ?>
                 <div id=status>
+                    <div id=safe_btn>
+                    </div>
+                    <div id=help_btn>
+                    </div>
+                    <div id=volunteer_btn>
+                    </div>
                     
                 </div>
             </div>
@@ -56,42 +62,33 @@
             <h3>Photos</h3>
         </div>
         <div id=social_events>
-            <div id=affected>
-                <h2>Affected people detail
-            </div>
             <div id=help_requested>
                 <h2>Help requested people<h2>
-				<table border="1">
+                <table>
 				<?php
-					$query1='select NIC_num,first_name,last_name from civilian_detail ';
+					$query1='select NIC_num,first_name,last_name from civilian_detail';
 					$result1=$con->query($query1);
-					while($row1=$result1->fetch_assoc()){
-						
-						$sql="select * from disaster_events where event_id=".$_GET['event_id'];
-						$result2 = $con->query($sql);
-							while($row2=$result2->fetch_assoc()){
-								$nic_num=$row1["NIC_num"];
-								$text= $row2[$nic_num];
-								
-								if (strpos( $text,'not_requested') == false) {
-									$full_name=$row1["first_name"].' '.$row1["last_name"];
-									echo "<tr>";
-									echo "<td>{$full_name}</td>";
-									echo "</tr>";
-								}
-								
-							}
-						
+					while($civilian=$result1->fetch_assoc()){
+                        $nic_num=$civilian["NIC_num"];
+                        $help_request_status= explode(" ",$result[$nic_num])[1];
+                        if ($help_request_status=='requested') {
+                            $full_name=$civilian["first_name"].' '.$civilian["last_name"];
+                            echo "<tr>";
+                            echo "<td>{$full_name}</td>";
+                            echo "</tr>";
+                        }
 					}
 				?>
 				</table>
+            </div>
+            <div id=affected>
+                <h2>Affected people detail
             </div>
             <div id=organizations>
                 <h2>Organizations on action</h2>
             </div>
         </div>
     <script>
-        var status_btn = document.getElementById('status');
         var safe_status = '<?php echo $status[0]?>';
         var help_status = '<?php echo $status[1]?>';
         var volunteer_status = '<?php echo $status[2]?>';
@@ -103,15 +100,25 @@
             case 'not_set':
                 var html1 = "<button id='mark' onclick='markFun()'>Mark</button>";
         }
+        var safe_btn = document.getElementById('safe_btn');
+        safe_btn.innerHTML = html1;
+
         switch(help_status){
             case 'not_requested':
                 var html2 = "</br><form method=get action=request_help.php><button type=submit id='request_help' name=event_id value="+event_id+">Request help</button></form>";
+
+            case 'requested':
+                var html2 = "</br><form method=get action=request_help.php><button type=submit id='request_help' name=event_id value="+event_id+">Request help</button></form>"
         }
+        var help_btn = document.getElementById('help_btn');
+        help_btn.innerHTML = html2;
+
         switch(volunteer_status){
             case 'not_applied':
                 var html3 = "</br><form method=get action=volunteer_application.php><button id='volunteer' name=event_id value="+event_id+">Help others</button></form>";
         }
-        status_btn.innerHTML = ( html1 + html2 + html3 );
+        var volunteer_btn = document.getElementById('volunteer_btn');
+        volunteer_btn.innerHTML = html3;
 
         function markFun(){
             html1="<div class=switch_container><form method=post action=view_event.php><label class='switch'><input type=checkbox id=checkbox onclick=markingFun()><span class=slider></span></label><span class=indicator id=indicator>Safe</span><input  type=submit id=submit_btn value=submit></form></div>";
