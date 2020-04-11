e2984be392be4f8e067ed25d430dfc3ce71c2b0d
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-04-11 15:49:47 +0530

    Add files via upload

--- dbconfi/confi.php
+++ dbconfi/confi.php
@@ -1,4 +1,10 @@
 <?php
     $con = mysqli_connect("remotemysql.com","kfm2yvoF5R","4vkzHfeBh6") or die("Unable to connect");
     mysqli_select_db($con,"kfm2yvoF5R");
+
+    $org_DB = mysqli_connect("remotemysql.com","LvFAfm4fFA","JGhOtcM4ez") or die("Unable to connect");
+    mysqli_select_db($org_DB,"LvFAfm4fFA");
+
+    $notification_DB = mysqli_connect("remotemysql.com","LlvvpTpgPp","t6rKpohvCB") or die("Unable to connect");
+    mysqli_select_db($notification_DB,"LlvvpTpgPp");
 ?>
