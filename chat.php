0797136c3d856a350be53e463a7ff4c47fb29ead
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-04-13 00:11:52 +0530

    Add files via upload

--- chat.php
+++ chat.php
@@ -15,34 +15,37 @@
         <script>
             btnPress(6);
         </script>
-	<div id="container">
-	<h1>Group Disscussion</h1>
+        <div id="container">
+        <h1>Group Disscussion</h1>
 		<div id="feedback">
-		--
+		    --
 		</div>
 		
 		<form name=chat_form autocomplete="off">
-		<input type="text" name="msg_b" id="msg_b" placeholder="type massage here">   
-		<input type="hidden" name="name" id="name" value="<?php echo  $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>">
-        <input type="hidden" name="id" id="id" value="<?php echo  $_SESSION['user_nic']; ?>">            
-        
-		<button id="post_btn" type="button">Send</button>
-		<span id="status">-</span>
+            <input type="text" name="msg_b" id="msg_b" placeholder="type massage here">   
+            <input type="hidden" name="name" id="name" value="<?php echo  $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>">
+            <input type="hidden" name="id" id="id" value="<?php echo  $_SESSION['user_nic']; ?>">            
+            
+            <button id="post_btn" type="button">Send</button>
+            <span id="status">-</span>
 		
 		</form>
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 		<script>
+            var is_success = true;
+
 			$(document).ready(function(){
 				$("#msg_b").keypress(function()
 				{
 					$("#status").html("Typing Message...");
 				});
-			setInterval(function(){loadchats()},100);
+			setInterval(loadchats,100);
 			$("#post_btn").click(function(){
 				var name=$("#name").val();
 				var id=$("#id").val();
 				var message=$("#msg_b").val();
+                //console.log(name+' '+id+' '+message);
 				$.post("post.php",{name:name,message:message,id:id},function(data)
 				{
 					$("#status").html(data);
@@ -54,11 +57,18 @@
 			
 			function loadchats()
 			{
-				$.ajax({url:"chat_msg.php",success:function(result){
-					$("#feedback").html(result);
-				}});
+                if(is_success){
+                    is_success = false;
+                    $.ajax({url:"chat_msg.php",success:function(result){
+                        $("#feedback").html(result);
+                        
+                    },complete:function(){
+                        is_success = true;
+                    }
+                    });
+                }
 			}
-			</script>
+		</script>
 	</body>
 
 </html>
