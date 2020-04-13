bcaae30830403de2da8eb9058eebf321e8739c40
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-04-13 09:44:22 -0700

    Add files via upload

---
+++ org_join_leave.php
@@ -0,0 +1,54 @@
+<?php  
+    session_start();
+    require 'dbconfi/confi.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Join and Leave</title>
+    </head>
+
+    <body>
+        <?php
+            if($_SERVER['REQUEST_METHOD']=='GET'){
+                $org_id=$_GET['edit_detail'];
+            }
+            $nic=$_SESSION['user_nic'];    
+            $value="null";
+
+           $query="select * from organizations where org_id=".$org_id;
+            $result=($con->query($query))->fetch_assoc();
+            $status=explode(" ",$result['members']);
+            $leader=$result['leader'];
+            $co_leader=$result['co_leader'];
+
+            if ($leader===$nic ){
+                $query="UPDATE `organizations` SET `leader` = '".$value."' where org_id='$org_id'";
+            }
+            elseif($co_leader===$nic){
+                $query="UPDATE `organizations` SET `co_leader` = '".$value."' where org_id='$org_id'";
+
+            }
+            elseif(in_array($nic, $status)){
+                $key = array_search($nic,$status);
+                unset($status[$key]);
+                $my=join(" ",$status);
+                $query="UPDATE `organizations` SET `members` = '".$my."' where org_id='$org_id'";
+            }else{
+                array_push($status,$nic);
+                $my=join(" ",$status);
+                $query="UPDATE `organizations` SET `members` = '".$my."' where org_id='$org_id'";
+            }   
+
+            $query_run= mysqli_query($con,$query);
+            if($query_run ){
+                echo '<script type="text/javascript"> alert ("Data Uploaded") </script>';
+                header('location:view_org.php?selected_org='.$org_id);
+            }
+        ?>
+    </body>
+</html>
+
+
+
