edd0eb31362887075a2d65880db2787f35c510ff
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-06-07 20:50:17 +0530

    Add files via upload

--- organization/promise.php
+++ organization/promise.php
@@ -1,7 +1,10 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
 require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
-$my_promises='';
+$org_id= $_GET['org_id'];
+$event_id=$_GET['event_id'];
+$event_name1="event_".$event_id."_help_requested";
+$event_name2="event_".$event_id."_pro_don";
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,7 +37,7 @@ $my_promises='';
         //foreach($str_arr as $NIC) {
         //   print($NIC);
         //}
-        $query="select * from event_2_help_requested ";
+        $query="select * from $event_name1 ";
         $result=$con->query($query);
         if($result->num_rows>0){
             while($row=$result->fetch_assoc()){
@@ -55,7 +58,7 @@ $my_promises='';
 
                     echo "<td>";
 
-                    $query4="select * from event_2_pro_don where to_person='$idd' ORDER BY by_org";
+                    $query4="select * from $event_name2 where to_person='$idd' ORDER BY by_org";
                     $result4=$con->query($query4);
                     if($result4->num_rows>0){
                         while($rows=$result4->fetch_assoc()){
@@ -65,12 +68,14 @@ $my_promises='';
                                 $result2=($con->query($query2))->fetch_assoc();
                                 echo "<b>".$result2['first_name']." ".$result2['last_name']."</b>";
                                 echo "<br>";
-                                $string1 = explode (",", $rows['content']);
-                                foreach($string1 as $str) {
-                                    $string2 = explode (":", $str);
-                                    echo "\t".$string2[0]." ".$string2[1];
-                                    echo "<br>";
-                                }
+								if ($rows['content'] !=""){
+									$string1 = explode (",", $rows['content']);
+									foreach($string1 as $str) {
+										$string2 = explode (":", $str);
+										echo "\t".$string2[0]." ".$string2[1];
+										echo "<br>";
+									}
+								}
                             }
                             else{
                                 $o_id=$rows['by_org'];
@@ -78,12 +83,14 @@ $my_promises='';
                                 $result3=($con->query($query3))->fetch_assoc();
                                 echo "<b>".$result3['org_name']."</b>";
                                 echo "<br>";
+								if ($rows['content'] !=""){
                                 $string1 = explode (",", $rows['content']);
-                                foreach($string1 as $str) {
-                                    $string2 = explode (":", $str);
-                                    echo "\t".$string2[0]." ".$string2[1];
-                                    echo "<br>";
-                                }
+									foreach($string1 as $str) {
+										$string2 = explode (":", $str);
+										echo "\t".$string2[0]." ".$string2[1];
+										echo "<br>";
+									}
+								}
                             }
                             echo "<br>";
                         }
@@ -93,6 +100,8 @@ $my_promises='';
 
 
                     echo "</td>";
+					
+					echo "<input type='hidden' id='hidden' name='hidden' value=$idd >";
 
                     echo "<td class='your_promise'>";
                     ?>
@@ -117,12 +126,12 @@ $my_promises='';
         ?>
         <tr>
             <td colspan='5'>
-                <input type='button' name='submit' id='submit' value='submit'/>
+                <button id="submit_btn" onclick="submit_all()">submit all</button>
             </td>
         </tr>
     </table>
 </div>
-<input type='hidden' id='hidden' name='hidden'>
+
 
 <script>
     function add_input(element){
@@ -155,6 +164,36 @@ $my_promises='';
             td.firstElementChild.value = ele.value;
         }
     }
+	function submit_all(){
+        var all_td = document.getElementsByClassName("your_promise");
+		
+		var arr= new Array() ;  		
+		for(var td of all_td){
+            var idd=td.previousElementSibling.value;
+			var promise='';
+			for(var tdd of td.children){
+				var val1= tdd.children[0].value;
+				var val2= tdd.children[1].value;
+				if(val1 != "" ){
+					if(val1 != null){
+						promise += val1+":"+val2+",";
+					}
+				}
+			}
+			var promise = promise.slice(0,promise.length-1);
+			var note=td.nextElementSibling.firstElementChild.value;
+			var data= idd+"--"+promise+"--"+note;
+			arr.push(data);
+			
+        }
+        document.getElementById('datas').value=arr.join("++");
+		document.getElementById("myForm").submit();
+    }
 </script>
+	<form id="myForm" action="submit.php" method=post>
+	    <input type="hidden" name="datas" id="datas"><br>
+	    <input type="hidden" name="org_id" value=<?php echo $org_id; ?> ><br>
+		<input type="hidden" name="event_id" value=<?php echo $event_id; ?> ><br>
+	</form>
 </body>
 </html>
