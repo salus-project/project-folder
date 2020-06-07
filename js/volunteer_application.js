9620a345f16df9f09bbe5722df4630371b02c2f3
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-06-06 22:33:23 -0700

    Add files via upload

--- js/volunteer_application.js
+++ js/volunteer_application.js
@@ -58,15 +58,16 @@ function add_option(){
     var table = document.getElementById("volunteer_table");
     table.innerHTML+=   "<tr>"+
                             "<td class=des_area>"+
-                                "<div id=money_des_con style='display:block'>"+
-                                    '<textarea cols="15" rows="1"  class="input_box" name="money_description" id="money_des"></textarea>'+
+                                "<div>"+
+                                    '<input class="input_box" name="things" ></input>'+
                                 "</div>"+
                             "</td>"+
                             "<td class=des_area>"+
-                                '<div id=money_des_con style="display:block">'+
-                                    '<textarea cols="15" rows="1"  class="input_box" name="money_description" id="money_des"></textarea>'+
+                                '<div>'+
+                                    '<input class="input_box" name="quantity"></input>'+
                                 "</div>"+
-                            "</td>";
+                            "</td>"+
+                            "<tr>";
 }
 function show_menu() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -84,4 +85,23 @@ window.onclick = function(event) {
             }
         }
     }
+}
+
+function add_input(element){
+    var parent = element.parentElement.parentElement;
+    if(element.parentElement.children[0].value!=='' || element.parentElement.children[1].value!=='') {
+        for (var ele of parent.children) {
+            ele.children[0].setAttribute("value", ele.children[0].value);
+            ele.children[1].setAttribute("value", ele.children[1].value);
+            ele.children[2].outerHTML = "<button type='button' onclick='remove_input(this)' class='add_rem_btn'>Remove</button>"
+        }
+        parent.innerHTML += '<div class="input_sub_container">\n' +
+            '        <input type="text"  name="thing_type[]" class="text_input request_input">\n' +
+            '        <input type="text"   name="quantity[]" class="text_input request_input" >\n' +
+            '        <button type="button" onclick="add_input(this)" class="add_rem_btn">Add</button>\n' +
+            '    </div>';
+    }
+}
+function remove_input(element){
+    element.parentElement.outerHTML='';
 }
