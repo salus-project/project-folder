832133d43031a0f7c051fd4016b4a37d44bc0b14
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-06-09 12:22:50 +0530

    Add files via upload

--- event/help/index.php
+++ event/help/index.php
@@ -1,12 +1,12 @@
 <?php
-   // require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
+    require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
     require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
     $to=$_GET['to'];
     $event_id = $_GET['event_id'];
     $by=$_GET['by'];
     
     
-    $query = "select * from event_".$event_id."_help_requested where NIC_num = '".$to."'";
+    $query = "select event_".$event_id."_help_requested.*, civilian_detail.first_name, civilian_detail.last_name from event_".$event_id."_help_requested inner join civilian_detail on event_".$event_id."_help_requested.NIC_num = civilian_detail.NIC_num where event_".$event_id."_help_requested.NIC_num = '".$to."'";
     $result=$con->query($query)->fetch_assoc();
     
     if($result!=null){
@@ -21,6 +21,7 @@
 ?>
 
 <!DOCTYPE html>
+<html>
     <head>
         <link rel='stylesheet' type='text/css' href='/css_codes/help_event_individual.css'>
         <script src='/js/help_event_individual.js' type="text/javascript"></script>
@@ -29,11 +30,15 @@
 
         <div  id="form_box">
             <form method="POST" action=<?php echo "index_php.php";?>> 
-            <input type="hidden" name="event_id" value= <?php echo $event_id;?>>
-            <input type="hidden" name="to" value= <?php echo $to;?>>
-            <input type="hidden" name="by" value= <?php echo $by;?>>
+            <input type="hidden" name="event_id" value= '<?php echo $event_id;?>'>
+            <input type="hidden" name="to" value= '<?php echo $to;?>'>
+            <input type="hidden" name="by" value= '<?php echo $by;?>'>
             <div class=dis_container>
                 <table class="table1">
+                    <tr ><td colspan="2" id="requester_detail"><b>Requester
+                                <a class='all_a' href="/view_profile.php?id=<?php echo $result['NIC_num'] ?>">
+                                    <?php echo $result['first_name']." ".$result['last_name']?>
+                                </a><b></tr></td>
                     <tr ><td colspan="2" id="request_detail"><b>Requested details<b></tr></td>
                     <tr>
                         <td> District (current) :</td>
@@ -60,10 +65,9 @@
                             
                 </table>
             </div>
-            <?php      
-            echo $by;            
+            <?php
                 if($by=="your_self"){
-                    $data="SELECT * from event_".$event_id."_pro_don where by_person='199872641943' and to_person='".$to."'";     
+                    $data="SELECT * from event_".$event_id."_pro_don where by_person='{$_SESSION['user_nic']}' and to_person='".$to."'";
                 }else{
                     $by_org=$by;
                     $data="SELECT * from event_".$event_id."_pro_don where by_org='".$by_org."' and to_person='".$to."'";           
@@ -73,24 +77,36 @@
                     $result2=$result1->fetch_assoc();
 
                     echo '<div id="old_promise">';
-                        echo '<div class=head_label_container><label class="head_label"> <b>My promises</b> </label></div>';
+                        echo '<div class=head_label_container><label class="head_label">';
+                            if($by=="your_self"){
+                                echo "Promise on your behalf";
+                            }else{
+                                if(isset($_GET['org_name'])){
+                                    $org_name = $_GET['org_name'];
+                                }else{
+                                    $org_name = "your organization";
+                                }
+                                echo "Promise on behalf of ".$org_name;
+                            }
+                        echo '</label></div>';
                         $old_promise = $result2['content'];
                         $old_promises = array_filter(explode(",",$old_promise));
                         
                         foreach($old_promises as $row_req){
                             echo $row_req."<br>";
                         }
+                        echo "your note : ".($result2['note'] ?: "No notes");
                         echo '<div class="edit_button">';
-                        echo '<button type="button"  class="submit_button" onclick="edit_promise(this)" id=req_submit_btn >EDIT</button>';
+                            echo '<button type="button"  class="submit_button" onclick="edit_promise(this)" id=req_submit_btn >EDIT</button>';
                         echo '</div>';
                     echo '</div>';
 
-                    echo '<div class=dis_container id="promise_div">';
-                      /* echo '<div class=head_label_container id="old_donation"></div>';
+                    echo "<div id='promise_div' class='dis_container promise_div'>";
+                      echo '<div class=head_label_container id="old_donation">Edit your old promise</div>';
                         echo '<div class="input_container">';
                             foreach($old_promises as $row_req){
                                 $arr = explode(":",$row_req);
-                                echo "<div class=\"input_sub_container\">";
+                                echo "<div class='input_sub_container'>";
                                 echo    "<input type='text' class='text_input request_input' name='things[]' value='".$arr[0]."'>";
                                 echo    "<input type='text' class='text_input request_input' name='things_val[]' value='".$arr[1]."'>";
                                 echo    "<button type='button' onclick='remove_input(this)' class='add_rem_btn'>Remove</button>";
@@ -105,19 +121,31 @@
                         echo '</div>';
                         echo '<div id="promise_td">';
                             echo'<table>';
-                                echo '<tr><td><label name="note" id="note_label">Note</label></td><td><textarea col=30 rows=4 id="note" name="note"></textarea></td></tr>';
+                                echo '<tr><td><label name="note" id="note_label">Note</label></td><td><textarea col=30 rows=4 id="note" name="note">';
+                                    echo $result2['note'];
+                                echo '</textarea></td></tr>';
                             echo '</table>';
-
                         echo '</div>';                      
                         echo '<div class="pro_button">';
                             echo '<input name="edit_button" type="submit"  value="UPDATE PROMISE"  class="edit_button" id=req_submit_btn >';
-                        echo '</div>';*/
+                        echo '</div>';
                     echo '</div>';
                     }
                     else{                
-            
-                        echo '<div class=dis_container id="promise_div">';
-                            echo '<div class=head_label_container><label class="head_label"> <b>Donations<b> </label></div>';
+
+                        echo "<div id='promise_div' class='dis_container promise_div'>";
+                            echo '<div class=head_label_container><label class="head_label">';
+                                if($by=="your_self"){
+                                    echo "Promise on your behalf";
+                                }else{
+                                    if(isset($_GET['org_name'])){
+                                        $org_name = $_GET['org_name'];
+                                    }else{
+                                        $org_name = "your organization";
+                                    }
+                                    echo "Promise on behalf of ".$org_name;
+                                }
+                            echo '</label></div>';
 
                             echo '<div class="input_container">';
                             
