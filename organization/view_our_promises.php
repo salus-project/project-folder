4bc75da92e67fef846fbbde384363347a861443a
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-06-19 19:57:03 +0530

    Add files via upload

--- organization/view_our_promises.php
+++ organization/view_our_promises.php
@@ -5,6 +5,8 @@
     $event_name="event_".$event_id."_pro_don";
 	$query="select * from $event_name where by_org='$org_id'";
     $result=$con->query($query);
+	
+	$id_array = array(); 
 ?>
 
 
@@ -26,9 +28,11 @@
 		<div id='promise_body'>
             <table id='promise_table'>
             <thead>
+				<th colspan=1></th>
                 <th colspan=1>Person name</th>
                 <th colspan=1>Promises</th>
                 <th colspan=1>Note</th>
+				<th colspan=1></th>
              </thead>
 
             <?php
@@ -44,14 +48,79 @@
                 for ($x = 0; $x <$count_arr; $x++) {
                     $data=$data.$ability[$x]."<br>";
                 }
-
-                echo "<tr>
+				$row_id=$row['id'];
+                echo "<tr onclick='select(this.firstElementChild.firstElementChild)'>
+				<td><input type='checkbox' class='select_me' data-id='".$row_id."' id='edit_box' onclick='' ></td>
                 <td>".$name."</td><td>".$data."</td><td>".$row['note']."</td>
+				<td><form id='edit_me_form".$row_id."' class='edit_me' action='edit_me.php' method=post>
+				<input type='hidden' name='datas' ><br>
+				<input type='hidden' name='org_id' value= $event_id > 
+				<input type='hidden' name='event_id' value= $event_id > 
+				<input type='submit' name='submit_button'  value='Edit'>
+			</form></td>
                 </tr>";
+				array_push($id_array, $row_id);
             }
             ?>    
+			<tr>
+			<td>
+			<button type="button" onclick="select_all()" >select all</button>
+			<form id="edit_form" action="edit_our_promise.php" method=get>
+				<input type="hidden" name="id_list" id="id_list"><br>
+				<input type="hidden" name="org_id" value=<?php echo $org_id ; ?> >
+				<input type="hidden" name="event_id" value=<?php echo $event_id ; ?> >
+				<input type='submit' name='submit_button' id='submitBtn' value='Edit'>
+			</form>
+			</td><td></td><td></td><td></td><td></td>
+			</tr>
             </table>
         </div>
     </body>
+	<script>
+		var jArray = <?php echo json_encode($id_array); ?>;
+		var arr= new Array() ;  
+		function select_me(element){
+			var idn=element.getAttribute("data-id");
+			if(element.checked==true){
+				arr.push(idn);
+			}else{
+				for( var i = 0; i < arr.length; i++){ if ( arr[i] === idn) { arr.splice(i, 1); }}
+			}
+			document.getElementById('id_list').value=arr.join(",");
+			
+			console.log(arr);
+		}
+		function select(element){
+			if(element.checked==true){
+				element.checked=false;
+			}else{
+				element.checked=true;
+			}
+			select_me(element);
+		}	
+
+		function select_all(){
+			var element=document.getElementsByClassName('select_me');
+			var checked=true;
+			for(var td of element){
+				if (td.checked ==false){
+					checked=false;
+				}
+			}
+			if (checked==true){
+				arr =[];
+				for(var td of element){
+					td.checked =false;
+				}
+			}else{
+				arr=jArray;
+				for(var td of element){
+					td.checked =true;
+				}
+			}
+			document.getElementById('id_list').value=arr.join(",");
+		}
+	</script>
+
 
 </html>
