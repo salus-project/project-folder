2ba6a77448e42fffd5b2cda53db2d26af3459be6
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-06-23 23:59:16 +0530

    Add files via upload

--- organization/event/promise.php
+++ organization/event/promise.php
@@ -4,6 +4,8 @@ $org_id= $_GET['org_id'];
 $event_id=$_GET['event_id'];
 $event_name1="event_".$event_id."_help_requested";
 $event_name2="event_".$event_id."_pro_don";
+$event_name3="event_".$event_id."_pro_don_content";
+$event_name4="event_".$event_id."_requests";
 
 function console_log($data) {
     $output = $data;
@@ -48,32 +50,7 @@ function console_log($data) {
         $array1=[];
         $array2=[];
         $array3=[];
-        $array4=[];
-        $query1="select * from $event_name2 WHERE by_org=$org_id";
-        $result1=$con->query($query1);
-        if($result1->num_rows>0){
-            while($row1=$result1->fetch_assoc()){
-                if (in_array($row1["to_person"], $str_arr)) {
-                    if (in_array($row1["to_person"], $array1)) {
-                        $key = array_search($row1["to_person"], $array1); 
-                        $item=$row1["item"].":".$row1["amount"];
-                        $array2[$key]=$array2[$key].",".$item;
-                        $array3[$key]=$array3[$key].",".$row1["id"];
-                    }else{
-                        array_push( $array1,$row1["to_person"]);
-                        $item=$row1["item"].":".$row1["amount"];
-                        array_push( $array2,$item);
-                        array_push( $array3,$row1["id"]);
-                        array_push( $array4,$row1["note"]);
-                    }
-                }
-            }
-        }
-        // console_log($array1);
-        // console_log($array2[0]);
-        // console_log($array3);
-        // console_log($array4);
-
+    
         $query="select * from $event_name1 ";
         $result=$con->query($query);
         if($result->num_rows>0){
@@ -86,55 +63,111 @@ function console_log($data) {
                     echo"<td>".$result1['first_name']." ".$result1['last_name']."</td>";
                     echo"<td>".$row['street']." ".$row['village']." ".$row['district']."</td>";
 
-                    $request = explode(",", $row['requests']);
+                    
                     echo"<td>";
-                    foreach($request as $res){
-                        echo "$res <br>";
-                    }
-                    echo"</td>";
-
-                    echo "<td>";
-
-                    $query4="select * from $event_name2 where to_person='$idd' ORDER BY by_org";
+                    $query4="select * from $event_name4 where requester='$idd'";
                     $result4=$con->query($query4);
                     if($result4->num_rows>0){
-                        while($rows=$result4->fetch_assoc()){
-                            if ($rows['by_org']==""){
-                                $iddd=$rows['by_person'];
-                                $query2="select * from civilian_detail where NIC_num='$iddd'";
-                                $result2=($con->query($query2))->fetch_assoc();
-                                echo "<b>".$result2['first_name']." ".$result2['last_name']."</b>";
-                                echo "<br>";
-								if ($rows['content'] !=""){
-									$string1 = explode (",", $rows['content']);
-									foreach($string1 as $str) {
-										$string2 = explode (":", $str);
-										echo "\t".$string2[0]." ".$string2[1];
-										echo "<br>";
-									}
-								}
+                        while($row4=$result4->fetch_assoc()){
+                            echo $row4["item"]." ".$row4["amount"]."<br>";
+                        }
+                    }
+                
+                    echo"</td>";
+                    
+                    $org =[];
+                    $org_i=[];
+                    $person=[];
+                    $person_i=[];
+                    $our=[];
+                    $note='';
+                    $query1="SELECT a.by_org AS org, a.id AS id_d, a.by_person AS person, a.note AS note, b.id AS ids, b.item AS item, b.amount AS amount FROM $event_name2 AS a INNER JOIN $event_name3 AS b ON (a.id = b.don_id) WHERE a.to_person = '$idd'";
+                    $result1=$con->query($query1);
+                    if($result1->num_rows>0){
+                        while($row1=$result1->fetch_assoc()){
+                                if($row1['person']==""){
+                                    if($row1['org']==$org_id){
+                                        array_push( $our,$row1['item'].':'.$row1['amount']);
+                                        $note=$row1['note'];
+                                        if (in_array($idd, $array1)) {
+                                            $key = array_search($idd, $array1); 
+                                            $array2[$key]=$array2[$key].",".$row1['ids'];
+                                        }else{
+                                            array_push( $array1,$idd);
+                                            array_push( $array3,$row1['id_d']);
+                                            array_push( $array2,$row1['ids']);
+                                        }
+                                    }
+                                    else{
+                                        if (in_array($row1['org'], $org)) {
+                                            $key = array_search($row1['org'], $org); 
+                                            $org_i[$key]=$org_i[$key].",".$row1['item'].":".$row1['amount'];
+                                        }
+
+                                        else{
+                                            array_push( $org,$row1['org']);
+                                            array_push( $org_i,$row1['item'].":".$row1['amount']);
+
+                                        }
+                                    }
+                                }
+                                else{
+                                    if (in_array($row1['person'], $person)) {
+                                        $key = array_search($row1['person'], $person); 
+                                        $person_i[$key]=$person_i[$key].",".$row1['item'].":".$row1['amount'];
+                                    }
+
+                                    else{
+                                        array_push( $person,$row1['person']);
+                                        array_push( $person_i,$row1['item'].":".$row1['amount']);
+
+                                    }
+                                }
+                                }
                             }
-                            else{
-                                $o_id=$rows['by_org'];
-                                $query3="select * from organizations where org_id=".$o_id;
-                                $result3=($con->query($query3))->fetch_assoc();
-                                echo "<b>".$result3['org_name']."</b>";
-                                echo "<br>";
-								if ($rows['content'] !=""){
-                                $string1 = explode (",", $rows['content']);
-									foreach($string1 as $str) {
-										$string2 = explode (":", $str);
-										echo "\t".$string2[0]." ".$string2[1];
-										echo "<br>";
-									}
-								}
-                            }
+
+                    echo "<td>";
+                    // console_log($array1);
+                    // console_log($array2);
+                    // console_log($our);
+                    // console_log($org);
+                    // console_log($org_i);
+                    // console_log($person);
+                    // console_log($person_i);
+                    $i=0;
+                    foreach($org as $_org){
+                        $query2="select * from organizations where org_id=".$_org;
+                        $result2=($con->query($query2))->fetch_assoc();
+                        echo "<b>".$result2['org_name']."</b>";
+                        echo "<br>";
+                        $string1 = explode (",", $org_i[$i]);
+                        foreach($string1 as $str) {
+                            $string2 = explode (":", $str);
+                            echo "\t".$string2[0]." ".$string2[1];
                             echo "<br>";
                         }
-                    }else{
-                        echo "<b>No Promises<b>";
+                        $i += 1;
+                        echo "<br>";
                     }
 
+                    $i=0;
+                    foreach($person as $_per){
+                        $query2="select * from civilian_detail where NIC_num='$_per'";
+                        $result2=($con->query($query2))->fetch_assoc();
+                        echo "<b>".$result2['first_name']." ".$result2['last_name']."</b>";
+                        echo "<br>";
+                        $string1 = explode (",", $person_i[$i]);
+                        foreach($string1 as $str) {
+                            $string2 = explode (":", $str);
+                            echo "\t".$string2[0]." ".$string2[1];
+                            echo "<br>";
+                        }
+                        $i += 1;
+                        echo "<br>";
+                    }
+                    if (empty($org) and empty($person)){
+                        echo "<b>No promises</b>";
+                    }
 
                     echo "</td>";
 					
@@ -142,30 +175,25 @@ function console_log($data) {
 
                     echo "<td class='your_promise' data-id='".$idd."'>";
 
-                    $note='';
                     echo "<div class='input_container'>";
-                    if (in_array($idd, $array1)) { 
-                        $key = array_search($idd, $array1);
-                        $note=$array4[$key]; 
-                        $str_items=$array2[$key];
-                        $arr_items = explode (",", $str_items);
-                            foreach($arr_items as $str) {
-                                $stri = explode (":", $str);
-                                echo "<div class='input_sub_container'>";
-                                echo "<input type='text' class='text_input' value='".$stri[0]."'>";
-                                echo "<input type='text' class='text_input'  value='".$stri[1]."'>";
-                                echo "<button type='button' onclick='remove_input(this)' class='add_rem_btn'>Remove</button>";
-                                echo "</div>";
-                            }
+                    if (!empty($our)) { 
+                        foreach($our as $str) {
+                            $stri = explode (":", $str);
+                            echo "<div class='input_sub_container'>";
+                            echo "<input type='text' class='text_input' value='".$stri[0]."'>";
+                            echo "<input type='text' class='text_input'  value='".$stri[1]."'>";
+                            echo "<button type='button' onclick='remove_input(this)' class='add_rem_btn'>Remove</button>";
+                            echo "</div>";
+                        }
                     }
-                        echo "<div class='input_sub_container'>";
-                        echo "<input type='text' class='text_input'>";
-                        echo "<input type='text' class='text_input'>";
-                        echo "<button type='button' onclick='add_input(this)' class='add_rem_btn'>Add</button>";
-                        echo "</div>";
+                    
+                    echo "<div class='input_sub_container'>";
+                    echo "<input type='text' class='text_input'>";
+                    echo "<input type='text' class='text_input'>";
+                    echo "<button type='button' onclick='add_input(this)' class='add_rem_btn'>Add</button>";
                     echo "</div>";
-
-                  
+                    
+                    echo "</div>";
                     echo "</td>";
                     echo "<td class='note_td'>";
                     echo "<textarea type=text name=note class=note >".$note."</textarea>";
@@ -238,7 +266,7 @@ function console_log($data) {
 			
         }
         document.getElementById('datas').value=arr.join("++");
-        console.log(arr.join("++"))
+        //console.log(arr.join("++"))
 		document.getElementById("myForm").submit();
     }
 </script>
@@ -247,7 +275,8 @@ function console_log($data) {
 	    <input type="hidden" name="org_id" value=<?php echo $org_id; ?> ><br>
 		<input type="hidden" name="event_id" value=<?php echo $event_id; ?> ><br>
         <input type="hidden" name="array1" value=<?php echo implode(",",$array1); ?> ><br>
-        <input type="hidden" name="array3" value=<?php echo implode("++",$array3); ?> ><br>
+        <input type="hidden" name="array2" value=<?php echo implode("++",$array2); ?> ><br>
+        <input type="hidden" name="array3" value=<?php echo implode(",",$array3); ?> ><br>
 	</form>
 </body>
 </html>
--- organization/event/submit.php
+++ organization/event/submit.php
@@ -5,69 +5,82 @@ require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
 	$string1= $_POST["datas"];
 	$org_id= $_POST["org_id"];
 	$event_id= $_POST["event_id"];
-	$event_name="event_".$event_id."_pro_don";
+	$event_name1="event_".$event_id."_pro_don";
+	$event_name2="event_".$event_id."_pro_don_content";
 	$string2 = explode ("++", $string1);
 	$array1=explode (",", $_POST["array1"]);
-	$array3=explode ("++", $_POST["array3"]);
+	$array3=explode (",", $_POST["array3"]);
+	$array2=explode ("++", $_POST["array2"]);
 	
+
 	foreach($string2 as $str_data) {
 		$str = explode ("--", $str_data);
 		if (in_array($str[0], $array1)){
 			$key = array_search($str[0], $array1); 
-			$ids=explode (",", $array3[$key]);
+			$query="UPDATE $event_name1 SET `note` = '$str[2]' WHERE `id` = '$array3[$key]';";
+			$query_run=mysqli_query($con,$query);
+			
+
+			$ids=explode (",", $array2[$key]);
 			$str_items=explode (",", $str[1]);
 			$i=0;
+
 			if (sizeof($str_items)==sizeof($ids)){
 				foreach($str_items as $items){
 					$item=explode (":", $items);
-					$query="UPDATE $event_name SET `item` = '$item[0]', `amount` = '$item[1]', `note` = '$str[2]' WHERE `id` = '$ids[$i]';";
+					$query="UPDATE $event_name2 SET `item` = '$item[0]', `amount` = '$item[1]' WHERE `id` = '$ids[$i]';";
 					$query_run=mysqli_query($con,$query);
 					$i=$i+1;
 				}
 			}
+
 			if (sizeof($str_items) < sizeof($ids)){
 				foreach($str_items as $items){
 					$item=explode (":", $items);
-					$query="UPDATE $event_name SET `item` = '$item[0]', `amount` = '$item[1]', `note` = '$str[2]' WHERE `id` = '$ids[$i]';";
+					$query="UPDATE $event_name2 SET `item` = '$item[0]', `amount` = '$item[1]' WHERE `id` = '$ids[$i]';";
 					$query_run=mysqli_query($con,$query);
 					$i=$i+1;
 				}
 				while (sizeof($ids) > $i ) {
-					$sql = "DELETE FROM $event_name WHERE id=$ids[$i]";
+					$sql = "DELETE FROM $event_name2 WHERE id=$ids[$i]";
 					$con->query($sql); 
 					$i=$i+1;
 				  }
-
 			}
 			if (sizeof($str_items) > sizeof($ids)){
 				foreach($str_items as $items){
 					$item=explode (":", $items);
 					if(sizeof($ids) > $i ){
-					$query="UPDATE $event_name SET `item` = '$item[0]', `amount` = '$item[1]', `note` = '$str[2]' WHERE `id` = '$ids[$i]';";
-					$query_run=mysqli_query($con,$query);
-					$i=$i+1;
+						$query="UPDATE $event_name2 SET `item` = '$item[0]', `amount` = '$item[1]' WHERE `id` = '$ids[$i]';";
+						$query_run=mysqli_query($con,$query);
+						$i=$i+1;
 					}
 					else{
-						if ($item[0]!="" or $item[1]!=""){
-							$query="INSERT INTO `$event_name` (`pro_don`, `by_org`, `to_person`,`item`,`amount`,`note` ) VALUES ( 'promise', '$org_id', '$str[0]', '$item[0]', '$item[1]', '$str[2]')";
-							$query_run=mysqli_query($con,$query);
-						}
+						$query="INSERT INTO `$event_name2` (`don_id`,`item`,`amount` ) VALUES (  '$array3[$key]', '$item[0]', '$item[1]')";
+						$query_run=mysqli_query($con,$query);
 					}
 				}
 			}
-		}
-		else{
+			
+		}else{
 			$str_items=explode (",", $str[1]);
-			foreach($str_items as $items){
+
+			$sub_query = "INSERT INTO `$event_name2`(`don_id`,`item`,`amount`) VALUES ";
+			$querry_arr = array();
+			foreach ($str_items as $items) {
 				$item=explode (":", $items);
-				if ($item[0]!="" or $item[1]!=""){
-					$query="INSERT INTO `$event_name` (`pro_don`, `by_org`, `to_person`,`item`,`amount`,`note` ) VALUES ( 'promise', '$org_id', '$str[0]', '$item[0]', '$item[1]', '$str[2]')";
-					$query_run=mysqli_query($con,$query);	
-				}
+			  array_push($querry_arr, "(last_insert_id(),'$item[0]','$item[1]')");
 			}
+	  
+			$sub_query = $sub_query . implode(", ", $querry_arr).";";
+	  
+			$query = "INSERT INTO `$event_name1` (`pro_don`, `by_org`, `to_person`,`note` ) VALUES ( 'promise', '$org_id', '$str[0]', '$str[2]');".$sub_query;
+			mysqli_multi_query($con,$query);
+			
 		}
 	}
+		
 	$location="?event_id=".$event_id."&selected_org=".$org_id."";
-	header("Location:".$location."");
+	//header("Location:".$location."");
 	
 ?>
