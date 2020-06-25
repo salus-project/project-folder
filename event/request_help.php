1b22975b45732e9ddf1884dec6a05365c900fadf
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-06-25 23:28:35 +0530

    Add files via upload

--- event/request_help.php
+++ event/request_help.php
@@ -8,88 +8,67 @@
             $data = stripslashes($data);
             $data = htmlspecialchars($data);
             return $data;
-        }        
-         
-        $district=$village= $street='';
-        $district_err=$village_err= $street_err='';
-        $is_ok =true;
-        if (empty($_POST['district'])){
-            $district_err = "District is required";
-            $is_ok =  false;
-        } else {
-            $district = test_input($_POST['district']);
+        }
+        
+        $district = test_input($_POST['district']);
+        $village = test_input($_POST['village']);
+        $street = test_input($_POST['street']);
+        
+        $event_id=$_POST['event_id'];
+        $user_nic=$_SESSION['user_nic'];
+        $item = array_filter($_POST['item']);
+        $amount = $_POST['amount'];
+        $update_id=$_POST['update_id'];
+
+        $pri_query = '';
+
+        if($_POST['entry_update_id']!=0){
+            $del_detail = array_filter(explode(',', $_POST['del_details']));
+            foreach( $del_detail as $row_del){
+                $pri_query.= "delete from event_".$event_id."_requests where id=$row_del;";
+            }
+            for($x=0 ; $x < count($item) ; $x++){
+                if(!empty($item[$x])){
+                    if(empty($amount[$x])){
+                        $amount[$x]=0;
+                    }
+                    if($update_id[$x]==0){
+                        $pri_query .= "INSERT INTO event_".$event_id."_requests (requester, item, amount) VALUES ('$user_nic', '".test_input($item[$x])."', '".test_input($amount[$x])."');";
+                    
+                    }else{
+                        $pri_query .= "UPDATE `event_".$event_id."_requests` SET `item` = '".test_input($item[$x])."', `amount` = '".test_input($amount[$x])."' WHERE `event_2_requests`.`id` = ".$update_id[$x].";";
+                    }
+                }
+            }
+            $pri_query.= "UPDATE `event_".$event_id."_help_requested` SET `now` = 'yes' WHERE `NIC_num` = '".$user_nic."';";
+                
+        }else{
+        $pri_query= "INSERT INTO event_".$event_id."_help_requested (NIC_num, district, village, street) VALUES ('$user_nic', '$district', '$village', '$street');";
+        if(count($item)>0){
+                $querry_arr = array();
+                for($x=0; $x < count($item); $x++ ){
+                    $row_item = $item[$x]?test_input($item[$x]):'NULL';
+                    $row_amount = $amount[$x]?test_input($amount[$x]):'0';
+                    array_push($querry_arr, "('$user_nic','$row_item','$row_amount')");
+                }
+                $pri_query.= "INSERT INTO `event_".$event_id."_requests`(`requester`, `item`, `amount`) VALUES ". implode(", ", $querry_arr).";";
+            }      
         }
 
-
-        if (empty($_POST['village'])){
-            $village_err = "village is required";
-            $is_ok =  false;
-        } else {
-            $village = test_input($_POST['village']);
-        }
+        $status1=$_POST['status'];
+        $status=explode(" ",$status1);
+        $status[1]='requested';
+        $data1=join(" ",$status);
+        $pri_query.="UPDATE `disaster_events` SET `user_".$_SESSION['user_nic']."` = '".$data1."' WHERE `disaster_events`.`event_id` = $event_id;";
 
         
-        if (empty($_POST['street'])){
-            $street_err = "Street is required";
-            $is_ok =  false;
-        } else {
-            $street = test_input($_POST['street']);
+        if(mysqli_multi_query($con,$pri_query)){
+            header("location:".$_SERVER['HTTP_REFERER']);
         }
-        if($is_ok){
-            $event_id=$_POST['event_id'];
-            $user_nic=$_SESSION['user_nic'];
-            $item = array_filter($_POST['item']);
-            $amount = $_POST['amount'];
-            $update_id=array_filter($_POST['update_id']);
-
-            $pri_query = '';
-
-            if($_POST['entry_update_id']!=0){
-                $del_detail = array_filter(explode(',', $_POST['del_details']));
-                foreach( $del_detail as $row_del){
-                    $pri_query.= "delete from event_".$event_id."_requests where id=$row_del;";
-                }
-                for($x=0 ; $x < count($item) ; $x++){
-                    if(!empty($item[$x])){
-                        if(empty($amount[$x])){
-                            $amount[$x]=0;
-                        }
-                        if(test_input($update_id[$x])=='0'){
-                            $pri_query .= "INSERT INTO event_".$event_id."_requests (requester, item, amount) VALUES ('$user_nic', '$item[$x]', '$amount[$x]')";
-                        
-                        }else{
-                            $pri_query .= "UPDATE `event_".$event_id."_requests` SET `item` = '$item[$x]', `amount` = '$amount[$x]' WHERE `event_2_requests`.`id` = 'test_input(".$update_id[$x].")';";
-                        }
-                    }
-                }
-                $pri_query.= "UPDATE `event_".$event_id."_help_requested` SET `now` = 'yes' WHERE `NIC_num` = '".$user_nic."';";
-                    
-            }else{
-            $pri_query= "INSERT INTO event_".$event_id."_help_requested (NIC_num, district, village, street) VALUES ('$user_nic', '$district', '$village', '$street');";
-            if(count($item)>0){
-                    $querry_arr = array();
-                    for($x=0; $x < count($item); $x++ ){
-                        $row_item = $item[$x]?$item[$x]:'NULL';
-                        $row_amount = $amount[$x]?:'0';
-                        array_push($querry_arr, "('$user_nic','$row_item','$row_amount')");
-                    }
-                    $pri_query.= "INSERT INTO `event_".$event_id."_requests`(`requester`, `item`, `amount`) VALUES ". implode(", ", $querry_arr).";";
-                }      
-            }
-    
-            $status1=$_POST['status'];
-            $status=explode(" ",$status1);
-            $status[1]='requested';
-            $data1=join(" ",$status);
-            $pri_query.="UPDATE `disaster_events` SET `user_".$_SESSION['user_nic']."` = '".$data1."' WHERE `disaster_events`.`event_id` = $event_id";
-
-            if(mysqli_multi_query($con,$pri_query)){
-                echo "success";
-            }
-            else{
-                echo 'unsucessful in starting';
-            }
+        else{
+            echo 'unsucessful in starting';
         }
+        
     }
 
         
@@ -103,7 +82,7 @@
             UPDATE `event_".$event_id."_help_requested` SET `now` = 'no' WHERE `event_".$event_id."_help_requested`.`NIC_num` = '".$_SESSION['user_nic']."';";
 
         if(mysqli_multi_query($con,$pri_query)){
-            echo 'cancelled';
+            header("location:".$_SERVER['HTTP_REFERER']);
         }else{
             echo '<script type="text/javascript"> alert ("Not cancelled") </script>';
         }   
--- event/request_help_popup.php
+++ event/request_help_popup.php
@@ -4,10 +4,10 @@
     $event_id = $_GET['event_id'];
     $query = "select * from event_".$event_id."_help_requested where NIC_num = '".$_SESSION['user_nic']."';
     SELECT * from disaster_events where event_id='".$event_id."';
-    select * from event_".$event_id."_requests where requester = '".$_SESSION['user_nic']."'";
-   
+    select * from event_".$event_id."_requests where requester = '".$_SESSION['user_nic']."';";
+    
     if(mysqli_multi_query($con,$query)){
-        echo 'yes';
+        
         echo "<form method='post' action='request_help.php'>";
             echo "<input type='hidden' name='event_id' value='".$event_id."'>";
         $result=mysqli_store_result($con); 
@@ -88,26 +88,24 @@
         <?php
             foreach($old_content as $row_req){
                 echo "<div class=\"input_sub_container\">";
-                    echo    "<input type='text' class='text_input request_input' name='item[]' value='".$row_req['item']."'>
-                            <input type='text' class='text_input request_input' name='amount[]' value='".$row_req['amount']."'>";
-                    echo    "<button type='button' onclick='remove_request_input(this)' class='add_rem_btn'>Remove</button>";   
-                    echo "<input type='hidden' name='update_id[]' value='".$row_req['id']."'>";
+                echo    "<input type='text' class='text_input request_input' name='item[]' value='".$row_req['item']."'>
+                        <input type='text' class='text_input request_input' name='amount[]' value='".$row_req['amount']."'>";
+                echo    "<button type='button' onclick='remove_request_input(this)' class='add_rem_btn'>Remove</button>";      
+                echo    "<input type='hidden' name='update_id[]' value='".$row_req['id']."'>";
                 echo "</div>";
             }
-        
-            echo '<div class="input_sub_container">';
-                echo "<input type='text' name='item[]' class='text_input request_input'>";
-                echo "<input type='text' name='amount[]' class='text_input request_input'>";
-                echo '<button type="button" onclick="add_request_input(this)" class="add_rem_btn">Add</button>';
-                echo "<iput type='hidden' name='update_id[]' value='0'>";
-            echo "</div>";
         ?>
+        <div class="input_sub_container">
+            <input type="text" name='item[]' class='text_input request_input'>
+            <input type="text" name='amount[]' class='text_input request_input'>
+            <button type="button" onclick="add_request_input(this)" class="add_rem_btn">Add</button>
+            <input type='hidden' name='update_id[]' value='0'>
+        </div>
     </div>
     <div class=buttons>
         <input name="submit_button" type="submit"  value="Request"  class="submit_button" id=req_submit_btn onclick="submit_request(this.parentElement.parentElement)">
-        <button id=close_request_popup name='cancel_button' class=submit_button>Cancel</button>
-        <input id='del_details' type='hidden' value='' name='del_details'>
+        <button type='button' id=close_request_popup name='cancel_button' class=submit_button >Cancel</button>
     </div>
 </div>
-
+<?php echo "<input id='del_details' type='hidden' value='' name='del_details'>" ; ?>
 </form>
