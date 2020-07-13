679a9ed1b92d643aeed9472eaec3b004493657ea
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-07-13 20:35:31 +0530

    Add files via upload

--- organization/view_org-strategy.php
+++ organization/view_org-strategy.php
@@ -1,5 +1,5 @@
 <?php  
-    public class Viewer{
+    class Viewer{
         private $role;
 
         public function set_role($role){
@@ -11,104 +11,106 @@
         }
 
         public function display(){
-            $this->role->show_option();
+            $this->role->show_membership_button();
+            $this->role->show_edit_button();
         }
     }
 
 
-    public interface Role{
-        public show_option();
-    }
-
-    public class Visitor implements Role{
-        private $show_text;
+    abstract class Role{
+        private $join_text;
+        private $leave_text;
+        private $edit_text;
 
         public function __construct(){
-            $this->show_text =  "<div id=membership_btn_container>
+            $this->join_text =  "<div id=membership_btn_container>
                                     <form action=org_join_leave.php method=get>
                                         <button id=membership_btn type='submit' name=org_id value=".$_GET['selected_org'].">Join</button>
                                     </form>
                                 </div>";
-        }
-
-        public function show_option(){
-            echo $this->show_text;
-        }
-    }
-
-    public class Member implements Role{
-        private $show_text;
-
-        public function __construct(){
-            $this->show_text = "<div id=membership_btn_container>
+            $this->leave_text = "<div id=membership_btn_container>
                                     <form action=org_join_leave.php method=get>
                                         <button id=membership_btn type='submit' name=org_id value=".$_GET['selected_org'].">Leave</button>
                                     </form>
                                 </div>";
-        }
-
-        public function show_option(){
-            echo $this->show_text;
-        }
-    }
-
-    public class Co_leader implements Role{
-        private $show_text;
-
-        public function __construct(){
-            $this->show_text = "<div id=membership_btn_container>
-                                    <form action=org_join_leave.php method=get>
-                                        <button id=membership_btn type='submit' name=org_id value=".$_GET['selected_org'].">Leave</button>
-                                    </form>
-                                </div>
-
-                                <div id=edit_btn_container >
+            $this->edit_text= "<div id=edit_btn_container >
                                     <form action=edit_org.php method=get>
                                         <button id=edit_btn type='submit' name=edit_detail value=".$_GET['selected_org'].">Edit</button>
                                     </form>
                                 </div>";
         }
+        abstract function show_membership_button();
+        abstract function show_edit_button();
+    }
 
-        public function show_option(){
-            echo $this->show_text;
+    class Visitor extends Role{
+        public function __construct(){
+            parent::__construct();
+                                
+        }
+
+        public function show_membership_button(){
+            echo $this->join_text;
+        }
+        public function show_edit_button(){
+            echo '';
         }
     }
 
-    public class Leader implements Role{
-        private $show_text;
+    class Member extends Role{
 
         public function __construct(){
-            $this->show_text = "<div id=membership_btn_container>
-                                    <form action=org_join_leave.php method=get>
-                                        <button id=membership_btn type='submit' name=org_id value=".$_GET['selected_org'].">Leave</button>
-                                    </form>
-                                </div>
-
-                                <div id=edit_btn_container >
-                                    <form action=edit_org.php method=get>
-                                        <button id=edit_btn type='submit' name=edit_detail value=".$_GET['selected_org'].">Edit</button>
-                                    </form>
-                                </div>";
+            parent::__construct();
         }
 
-        public function show_option(){
-            echo $this->show_text;
+        public function show_membership_button(){
+            echo $this->leave_text;
+        }
+        public function show_edit_button(){
+            echo '';
         }
     }
 
-    
+    class Co_leader extends Role{
+
+        public function __construct(){
+            parent::__construct();
+        }
+
+        public function show_membership_button(){
+            echo $this->leave_text;
+        }
+        public function show_edit_button(){
+            echo $this->edit_text;
+        }
+    }
+
+    class Leader extends Role{
+
+        public function __construct(){
+            parent::__construct();
+        }
+
+        public function show_membership_button(){
+            echo $this->leave_text;
+        }
+        public function show_edit_button(){
+            echo $this->edit_text;
+        }
+    }
 
     if($text_role == "visiter"){
         $role = new Visitor();
     }elseif($text_role == "member"){
         $role = new Member();
-    }($text_role == "co-leader"){
-        $role = new C0_leader();
-    }($text_role == "leader"){
+    }elseif($text_role == "co-leader"){
+        $role = new Co_leader();
+    }elseif($text_role == "leader"){
         $role = new Leader();
     }
 
     $viewer = new Viewer();
     $viewer->set_role($role);
 
-    $viewer->display();
+    $viewer->display(); 
+?>
