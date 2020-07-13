9f7e42c321985d7dadc4f69434bf7dbadd04f5e3
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-07-13 23:47:59 +0530

    Add files via upload

--- fundraising/view_fundraising.php
+++ fundraising/view_fundraising.php
@@ -1,7 +1,46 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
-$query="select * from fundraisings where id=".$_GET['view_fun'];
-$result=($con->query($query))->fetch_assoc();
+    $query="select * from fundraisings where id=".$_GET['view_fun'].";
+    select * from civilian_detail where NIC_num=(
+        select by_civilian from fundraisings where id=".$_GET['view_fun']."
+    );
+    select org_name from organizations where org_id=(
+        select by_org from fundraisings where id=".$_GET['view_fun']."); 
+    select name from disaster_events where event_id=(
+        select for_event from fundraisings where id=".$_GET['view_fun'].");
+    select * from fundraisings_expects where fund_id=".$_GET['view_fun'].";";
+    if(mysqli_multi_query($con,$query)){
+        $result = mysqli_store_result($con);
+        $fundraising = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+
+        mysqli_next_result($con);
+        $result = mysqli_store_result($con);
+        $civi_detail = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+
+        mysqli_next_result($con);
+        $result = mysqli_store_result($con);
+        $org_name_result=mysqli_fetch_assoc($result);
+        $org_name_fundraising = (isset($org_name_result['org_name']))?$org_name_result['org_name']:'';
+        mysqli_free_result($result);
+
+        mysqli_next_result($con);
+        $result = mysqli_store_result($con);
+        $event_name_result=mysqli_fetch_assoc($result);
+        $event_name_fundraising = (isset($event_name_result['name']))?$event_name_result['name']:'';
+        mysqli_free_result($result);
+
+           
+        mysqli_next_result($con);
+        $result = mysqli_store_result($con);
+        $fund_expect = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        mysqli_free_result($result);
+        $expect="";
+        foreach($fund_expect as $row_req){
+            $expect.=$row_req['item']." : ".$row_req['amount']."<br>";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,81 +55,43 @@ $result=($con->query($query))->fetch_assoc();
             btnPress(7);
         </script>
         <div id="title">
-            <?php echo $result['name'] ?>
+            <?php echo '<center>'.$fundraising['name'].'</center>' ?>
         </div>
         <div id='fund_body'>
             <?php
-            if($result['by_civilian']==$_SESSION['user_nic']){
-                echo "<div id=fund_edit_btn_container >";
-                echo "<form action='/fundraising/edit_fundraising.php' method=get>";
-                echo "<button id=edit_btn type='submit' name=edit_btn value=".$_GET['view_fun'].">Edit</button>";
-                echo "</form>";
-                echo"</div>";
-            }
+                if($fundraising['by_civilian']==$_SESSION['user_nic']){
+                    echo "<div id=fund_edit_btn_container >";
+                    echo "<form action='/fundraising/edit_fundraising.php' method=get>";
+                    echo "<button id=edit_btn type='submit' name=edit_btn value=".$_GET['view_fun'].">Edit</button>";
+                    echo "</form>";
+                    echo"</div>";
+                }
             ?>
 
             <table id='fund_table'>
-                <tr>
-                    <td>Fundraising id </td>
-                    <td><?php echo $result['id'] ?></td>
-                </tr>
-
                 <?php
-
-                $idd= filter($result['by_civilian']);
-
-                $query3="select * from civilian_detail where NIC_num='$idd' ";
-                $result3=($con->query($query3))->fetch_assoc();
-                echo '<tr><td id=column1> Created by </td><td id=column2>' . $result3['first_name'] ." ". $result3['last_name']. '</td></tr>';
+                    echo '<tr><td id=column1> Created by </td><td id=column2>' . $civi_detail['first_name'] ." ". $civi_detail['last_name']. '</td></tr>';
 
 
-                if($result['by_org']!=NULL){
-                    $query1="select * from organizations where org_id=". $result['by_org'];
-                    $result1=($con->query($query1))->fetch_assoc();
-                    echo '<tr><td id=column1> Org name</td><td id=column2>' . $result1['org_name'] . '</td></tr>';
+                if($fundraising['by_org']!=NULL){
+                    echo '<tr><td id=column1> Org name</td><td id=column2>' . $org_name_fundraising . '</td></tr>';
                 }
-                if($result['for_event']==NULL){
-                    echo '<tr><td id=column1> Purpose</td><td id=column2>' . $result['for_any'] . '</td></tr>';
+                if($fundraising['for_event']==NULL){
+                    echo '<tr><td id=column1> Purpose</td><td id=column2>' . $fundraising['for_any'] . '</td></tr>';
                 }else{
-                    $query2="select * from disaster_events where event_id=". $result['for_event'];
-                    $result2=($con->query($query2))->fetch_assoc();
-                    echo '<tr><td id=column1>Purpose</td><td id=column2>' . $result2['name'] . '</td></tr>';
+                    echo '<tr><td id=column1>Purpose</td><td id=column2>' . $event_name_fundraising . '</td></tr>';
                 }
+                    echo '<tr><td id=column1> Expectations </td><td id=column2>' . $expect . '</td></tr>';
 
-                echo '<tr><td id=column1> Request</td><td id=column2>' . $result['type'] . '</td></tr>';
-
-                if($result['type']=="money only"){
-                    echo '<tr><td id=column1> Ammount in rs</td><td id=column2>' . $result['expecting_money'] . '</td></tr>';
-                }elseif($result['type']=="things only"){
-                    $things=explode(",",$result['expecting_things']);
-                    $count_arr = count($things);
-                    $data="";
-                    for ($x = 0; $x <$count_arr; $x++) {
-                        $data=$data.$things[$x]."<br>";
-                    }
-                    echo '<tr><td id=column1> Requested things </td><td id=column2>' . $data . '</td></tr>';
-                }else{
-                    $things=explode(",",$result['expecting_things']);
-                    $count_arr = count($things);
-                    $data="";
-                    for ($x = 0; $x <$count_arr; $x++) {
-                        $data=$data.$things[$x]."<br>";
-                    }
-                    echo '<tr><td id=column1> Ammount in rs</td><td id=column2>' . $result['expecting_money'] . '</td></tr>';
-                    echo '<tr><td id=column1> Requested things </td><td id=column2>' . $data . '</td></tr>';
-                }
-                function filter($input){
-                    return(htmlspecialchars(stripslashes(trim($input))));
-                }
                 ?>
 
                 <tr>
                     <td>Service area </td>
-                    <td><?php echo $result['service_area'] ?></td>
+                    <td><?php echo $fundraising['service_area'] ?></td>
                 </tr>
                 <tr>
                     <td>description </td>
-                    <td><?php echo $result['description'] ?></td>
+                    <td><?php echo $fundraising['description'] ?></td>
                 </tr>
 
             </table>
