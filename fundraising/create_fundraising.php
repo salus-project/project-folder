21810078ee2e2f821e8c2ff1ef14834f18b74899
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-07-14 08:32:23 +0530

    Add files via upload

--- fundraising/create_fundraising.php
+++ fundraising/create_fundraising.php
@@ -1,14 +1,22 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
+    require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
 
-$fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_area=$description="";
+    $fundraising_name=$org_name=$for_event=$for_any=$service_area=$description="";
+    $query="select * from org_members where (role='leader' or role='coleader') and NIC_num='".$_SESSION['user_nic']."';
+    select * from disaster_events;";
+    if(mysqli_multi_query($con,$query)){
+        $result = mysqli_store_result($con);
+        $org_result = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        mysqli_free_result($result);
 
-
-
+        mysqli_next_result($con);
+        $result = mysqli_store_result($con);
+        $event_name_result = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        mysqli_free_result($result);
+    }
 ?>
 
 <!DOCTYPE html>
-
 <html>
     <head>
         <title>Create new fundraising event</title>
@@ -19,6 +27,7 @@ $fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_ar
         <script>
             btnPress(7);
         </script>
+        <form method='POST' action='/fundraising/create_fundraising_php.php'>
         <div id="main_fund_form_body">
             <center><h2>Create a new fundraising event</h2></center>
             <small style="margin:10px;">Enter the details</small>
@@ -42,25 +51,12 @@ $fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_ar
                         <label for='organization'>Select organization</label>
                     </td>
                     <td>
-                        <select name="organization" id="org_num">
+                        <select name="organization" id="org_id">
                             <option value=''>Not organization based</option>
-                            <?php
-                            $query='select * from organizations';
-                            $result=$con->query($query);
-                            while($row=$result->fetch_assoc()){
-                                if($row["leader"]==$_SESSION['user_nic']){
-                                    echo "<option value=" . $row["org_id"] . ">" . $row["org_name"] . "</option>>";
-                                }else{
-                                    $co_leaders=$row["co_leader"];
-                                    $co_leader = explode(" ", $co_leaders);
-                                    foreach($co_leader as $leader){
-                                        if($leader==$_SESSION['user_nic']){
-                                            echo "<option value=" . $row["org_id"] . ">" . $row["org_name"] . "</option>>";
-                                            break;
-                                        }
-                                    }
+                            <?php                       
+                                foreach($org_result as $row){
+                                    echo "<option value=" . $row["org_id"] . ">" . $row["org_name"] . "</option>";
                                 }
-                            }
                             ?>
                     </td>
                 </tr>
@@ -70,85 +66,33 @@ $fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_ar
                     </td>
                     <td>
                         <input type="hidden" id="purp" name="purp" value="00" />
-                        <input type='radio' name="purpose" value=''  onclick='purposeFun()'>For event</br>
+                        <input type='radio' name="purpose" value=''  onclick='purposeFun()'>For event<br>
                         <select name="for_event" id="for_event"  style='display:none'>
                             <?php
-                            $query='select * from disaster_events';
-                            $result=$con->query($query);
-                            while($row=$result->fetch_assoc()){
-                                echo "<option value=" . $row["event_id"] . ">" . $row["name"] . "</option>>";
-                            }
+                                foreach($event_name_result as $row){
+                                    echo "<option value=" . $row["event_id"] . ">" . $row["name"] . "</option>";
+                                }
                             ?>
                             <div style="display:flex; height:20px;">
                                 <input type='radio' name="purpose" id='other_purpose_opt' value=''  onclick='purposeFun()'>Other purpose
                                 <input type='text' name='other_purpose' id='other_purpose' style='display:none' value=<?php echo $for_any; ?>>
                             </div>
 
-
-                            <script type="text/javascript">
-                                function purposeFun(){
-                                    if(document.getElementById("other_purpose_opt").checked){
-                                        document.getElementById('other_purpose').style.display='block'
-                                        document.getElementById('for_event').style.display='none'
-                                        document.getElementById("purp").value = "2";
-
-
-                                    }else{
-                                        document.getElementById('other_purpose').style.display='none'
-                                        document.getElementById('for_event').style.display='block'
-                                        document.getElementById("purp").value = "1";
-
-                                    }
-                                }
-                            </script>
                             <span id='error' class="error">* </span>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <label for='type'>Your Request</label>
+                        <label for='type'>Your Expectations</label>
                     </td>
                     <td>
-                        <input type="hidden" id="mon" name="mon" value="0" />
-                        <input type="hidden" id="thin" name="thin" value="0" />
-
-                        <input type="checkbox" id="money" onclick="checkmoneyfn()">Money
-                        <input type='text' name="expecting_money" id="expecting_money" placeholder='expected money in Rs' style='display:none' value=<?php echo $money; ?>>
-                        <input type="checkbox" id="things" onclick="checkthingsfn()">Things
-
-                        <div class="input_container" id=add_i style='display:none'>
+                        <div class="input_container" id=add_i >
                             <div class="input_sub_container">
-                                <input type="text" class="text_input">
-                                <input type="text" class="text_input">
+                                <input type="text" class="text_input" name="item[]" placeholder="item">
+                                <input type="text" class="text_input" name="amount[]" placeholder="amount">
                                 <button type="button" onclick="add_input(this)" class="add_rem_btn">Add</button>
                             </div>
                         </div>
-                        <input type='hidden' id='hidden' name='hidden'>
-                        <script>
-                            function checkmoneyfn(){
-                                if(document.getElementById("money").checked==true){
-                                    document.getElementById('expecting_money').style.display='block';
-                                    document.getElementById("mon").value = "1";
-
-                                }else{
-                                    document.getElementById('expecting_money').style.display='none';
-                                    document.getElementById("mon").value = "0";
-                                }
-                            }
-
-                            function checkthingsfn(){
-                                if(document.getElementById("things").checked==true){
-                                    document.getElementById('add_i').style.display='block';
-                                    document.getElementById("thin").value = "1";
-
-                                }else{
-                                    document.getElementById('add_i').style.display='none';
-                                    document.getElementById("thin").value = "0";
-                                }
-                            }
-
-                        </script>
-                        <span id='error' class="error">* </span>
                     </td>
                 </tr>
                 <tr>
@@ -169,18 +113,31 @@ $fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_ar
                 </tr>
                 <tr>
                     <td colspan='2'>
-                        <button  onclick=submit_data() id='submitBtn' >Submit</button>
+                        <input type="submit" name="submitBtn" id='submitBtn' value="SUBMIT"></button>
                     </td>
                 </tr>
             </table>
 
         </div>
-        <?php include $_SERVER['DOCUMENT_ROOT']."/includes/footer.php" ?>
+        </form>
     </body>
 
     <script>
+   
+        function purposeFun(){
+            if(document.getElementById("other_purpose_opt").checked){
+                document.getElementById('other_purpose').style.display='block'
+                document.getElementById('for_event').style.display='none'
+                document.getElementById("purp").value = "2";
 
-        //alert("hi");
+            }else{
+                document.getElementById('other_purpose').style.display='none'
+                document.getElementById('for_event').style.display='block'
+                document.getElementById("purp").value = "1";
+
+            }
+        }
+
         function add_input(element){
             var parent = element.parentElement.parentElement;
             if(element.parentElement.children[0].value!=='' || element.parentElement.children[1].value!=='') {
@@ -190,8 +147,8 @@ $fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_ar
                     ele.children[2].outerHTML = "<button type='button' onclick='remove_input(this)' class='add_rem_btn'>Remove</button>"
                 }
                 parent.innerHTML += '<div class="input_sub_container">\n' +
-                    '        <input type="text" class="text_input">\n' +
-                    '        <input type="text" class="text_input">\n' +
+                    '        <input type="text" name="item[]" class="text_input" placeholder="item">\n' +
+                    '        <input type="text" name="amount[]" class="text_input" placeholder="amount">\n' +
                     '        <button type="button" onclick="add_input(this)" class="add_rem_btn">Add</button>\n' +
                     '    </div>';
             }
@@ -199,47 +156,6 @@ $fundraising_name=$org_name=$type=$for_event=$for_any=$money=$things=$service_ar
         function remove_input(element){
             element.parentElement.outerHTML='';
         }
-
-
-        function submit_data(){
-            var all_td = document.getElementsByClassName("input_sub_container");
-
-            var arr= new Array() ;
-            var promise="" 	;
-            for(var td of all_td){
-                var val1= td.children[0].value;
-                var val2= td.children[1].value;
-                if(val1 != "" ){
-                    if(val1 != null){
-                        promise += val1+":"+val2+",";
-                    }
-                }
-            }
-            var promise = promise.slice(0,promise.length-1);
-            arr.push(document.getElementById("fun_name").value);
-            arr.push(document.getElementById("org_num").value);
-            arr.push(document.getElementById("for_event").value);
-            arr.push(document.getElementById("other_purpose").value);
-            arr.push(document.getElementById("purp").value);
-            arr.push(document.getElementById("mon").value);
-            arr.push(document.getElementById("thin").value);
-            arr.push(document.getElementById("expecting_money").value);
-            arr.push(promise);
-            arr.push(document.getElementById("service_area").value);
-            arr.push(document.getElementById("description").value);
-
-
-            //console.log(arr)
-
-            document.getElementById('list').value=arr.join("++");
-            document.getElementById("myForm").submit();
-        }
-
-
     </script>
-
-    <form id="myForm" action="create_fundraising_php.php" method=post>
-        <input type="hidden" name="list" id="list"  ><br>
-    </form>
 
 </html>
--- fundraising/create_fundraising_php.php
+++ fundraising/create_fundraising_php.php
@@ -1,125 +1,83 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
+    require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
 
-$string1= $_POST["list"];
-$string2 = explode ("++", $string1);
-
-
-$isOk=1;
-if(empty($string2[0])){
-    echo '<script type="text/javascript">alert("Fundraising event name is required")</script>';
-    $isOk=0;
-}else{
-    $fundraising_name=filter($string2[0]);
-    $validate_name_query="select * from fundraisings where name='$fundraising_name'";
-    $query_run=mysqli_query($con,$validate_name_query);
-    if(mysqli_num_rows($query_run)>0){
-        echo '<script type="text/javascript">alert("fundraising name already exits...")</script>';
-        $isOk=0;
+    $fundraising_name=filt_inp($_POST['fundraising_name']);
+    $for_event=$_POST['for_event']?$_POST['for_event']:'NULL';
+    $for_any=$_POST['other_purpose']?$_POST['other_purpose']:'NULL';
+    $item = array_filter($_POST['item']);
+    $amount = $_POST['amount'];
+    $service_area=$_POST['service_area'];
+    $description=$_POST['description'];
+    $by=$_SESSION['user_nic'];
+    if($_POST['organization']==""){
+        $org_id= "NULL";
     }
-}
-if($string2[1]==""){
-    $org_name= "NULL";
-}
-else{
-    $org_name=$string2[1];
-}
-
-$for_event=$string2[2];
-$for_any=$string2[3];
-
-$for_opt=$string2[4];
-$purpose='';
-if($for_opt=="00"){
-    echo '<script type="text/javascript">alert("fill purpose field")</script>';
-    $isOk=0;
-}
-elseif($for_opt=="1"){
-    $for_any=NULL;
-    $query2="select * from disaster_events where event_id=". $for_event;
-    $result2=($con->query($query2))->fetch_assoc();
-    $purpose=$result2['name'];
-}
-else{
-    $for_event="NULL";
-    $purpose=$for_any;
-    if(empty($for_any)){
-        echo '<script type="text/javascript">alert("purpose is required")</script>';
-        $isOk=0;
+    else{
+        $org_id=$_POST['organization'];
     }
-}
-$mon=$string2[5];
-$thin=$string2[6];
-
-
-if(($mon=="0") and ($thin=="0")){
-    echo '<script type="text/javascript">alert("select your requests")</script>';
-    $isOk=0;
-}
-elseif(($mon=="1") and ($thin=="1")){
-    $type="money and things";
-    $money=$string2[7];
-    $things=$string2[8];
-
-    if((empty($money)) or (empty($things))){
-        echo '<script type="text/javascript">alert("fill your request")</script>';
+    $isOk=1;
+    if(empty($_POST['fundraising_name'])){
+        echo '<script type="text/javascript">alert("Fundraising event name is required")</script>';
         $isOk=0;
-    }
-
-}
-elseif( $mon=="1"){
-    $type="money only";
-    $money=$string2[7];
-    $things=NULL;
-    if(empty($money)){
-        echo '<script type="text/javascript">alert("ammount is required")</script>';
-        $isOk=0;
-    }
-}
-elseif($thin=="1"){
-    $type="things only";
-    $money="NULL";
-    $things=$string2[8];
-
-    if(empty($things)){
-        echo '<script type="text/javascript">alert("things required")</script>';
-        $isOk=0;
-    }
-}
-
-$service_area=$string2[9];
-$description=$string2[10];
-$by=filter($_SESSION['user_nic']);
-
-// echo $fundraising_name.' '.$by.''.$org_name.' '.$type.' '. $for_event.' '.$for_any.' '. $money.' '.$things.' '.$service_area.' '.$description.' '.$last_id;
-if($isOk==1){
-    $query1="INSERT INTO public_posts (`author`, `org`, `date`) VALUES ('$by',$org_name,NOW())";
-    $con->query($query1);
-    $last_id=$con->insert_id;
-
-    $query="INSERT INTO `fundraisings` (`id`, `name`, `by_civilian`, `by_org`, `type`, `for_event`, `for_any`, `expecting_money`, `expecting_things`, `service_area`, `description`,`post_id`) VALUES (NULL, '$fundraising_name', '$by',$org_name, '$type', $for_event, '$for_any', $money, '$things','$service_area','$description',$last_id)";
-    $query_run=mysqli_query($con,$query);
-    $last_id2=$con->insert_id;
-
-
-    $query2="UPDATE public_posts set fund='$last_id2'   WHERE post_index=".$last_id;
-    $query_run2=mysqli_query($con,$query2);
-
-    if($query_run){
-
-        echo '<script type="text/javascript">alert("Successfully created")</script>';
-
     }else{
-        echo '<script type="text/javascript">alert("Error")</script>';
+        $validate_name_query="select * from fundraisings where name='$fundraising_name'";
+        $query_run=mysqli_query($con,$validate_name_query);
+        if(mysqli_num_rows($query_run)>0){
+            echo '<script type="text/javascript">alert("fundraising name already exits...")</script>';
+            $isOk=0;
+        }
     }
-}
-else{
-    echo "try again";
-}
+    
+    $for_opt=$_POST['purp'];
+    if($for_opt=="00"){
+        echo '<script type="text/javascript">alert("fill purpose field")</script>';
+        $isOk=0;
+    }
+    elseif($for_opt=="1"){
+        $for_any='NULL';
+    }
+    else{
+        $for_event='NULL';   
+    }
 
-header("Location:fundraising.php");
+    if($isOk==1){
+        $query="INSERT INTO public_posts (`author`, `org`, `date`) VALUES ('$by',$org_id,NOW())";
+        $con->query($query);
+        $last_id=$con->insert_id;
 
-function filter($input){
-    return(htmlspecialchars(stripslashes(trim($input))));
-}
+        $query1="INSERT INTO `fundraisings` (`name`, `by_civilian`, `by_org`, `for_event`, `for_any`, `service_area`, `description`,`post_id`) VALUES ('$fundraising_name', '$by',$org_id, $for_event, '$for_any', '$service_area','$description',$last_id)";
+        $query_run1=mysqli_query($con,$query1);
+        $last_id2=$con->insert_id;
+
+        $querry_arr=array();
+        $query2="UPDATE public_posts set fund='$last_id2' WHERE post_index=".$last_id.";";
+        for($x=0 ; $x < count($item) ; $x++){
+            if(!empty($item[$x])){
+                $item1=filt_inp(ready_input($item[$x]));
+                $amount1=filt_inp($amount[$x]);
+                if(empty($amount[$x])){
+                        $amount1=0;
+                        
+                }
+                array_push($querry_arr, "('$last_id2','".$item1."','$amount1')");
+            }
+        }
+        $query2.= "INSERT INTO `fundraisings_expects`(`fund_id`, `item`, `amount`) VALUES ". implode(",", $querry_arr).";";
+        
+        $query_run2=mysqli_multi_query($con,$query2);
+
+        if($query_run && $query_run1 && $query_run2){
+
+            echo '<script type="text/javascript">alert("Successfully created")</script>';
+
+        }else{
+            echo '<script type="text/javascript">alert("Error")</script>';
+        }
+    }
+    else{
+        echo "try again";
+    }
+
+   // header("Location:/fundraising");
+
 ?>
