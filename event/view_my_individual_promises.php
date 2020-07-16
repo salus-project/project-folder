e7e2b1d649fb6637422c75471a720afa2ecd2415
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-07-16 11:20:31 +0530

    Add files via upload

--- event/view_my_individual_promises.php
+++ event/view_my_individual_promises.php
@@ -1,125 +1,116 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
-require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
-$id=$_SESSION['user_nic'];
-$event_id = $_GET['event_id'];
-$query="select * from event_".$event_id."_pro_don inner join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.to_person where by_person='".$id."' AND (pro_don='promise' OR pro_don='pending');
-select * from event_".$event_id."_pro_don inner join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.to_person where by_person='".$id."' AND pro_don='donated' ;
-select * from event_".$event_id."_pro_don_content inner join event_".$event_id."_pro_don on event_".$event_id."_pro_don_content.don_id = event_".$event_id."_pro_don.id where by_person='".$id."';";
+    require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
+    require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
+    $id=$_SESSION['user_nic'];
+    $event_id = $_GET['event_id'];
+    $query="select * from event_".$event_id."_pro_don inner join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.to_person where by_person='".$id."';
+    select * from event_".$event_id."_pro_don_content inner join event_".$event_id."_pro_don on event_".$event_id."_pro_don_content.don_id = event_".$event_id."_pro_don.id where by_person='".$id."';";
 
-if(mysqli_multi_query($con,$query)){
-    $promise_person_detail=[];
-    $result=mysqli_store_result($con);
-    if(mysqli_num_rows($result)>0){
-        $promise_person_detail=mysqli_fetch_all($result,MYSQLI_ASSOC);
+    if(mysqli_multi_query($con,$query)){
+        $person_detail=[];
+        $result=mysqli_store_result($con);
+        if(mysqli_num_rows($result)>0){
+            $person_detail=mysqli_fetch_all($result,MYSQLI_ASSOC);
+        }
+   
+        $content_detail=[];
+        mysqli_next_result($con);
+        $result = mysqli_store_result($con);
+        $content_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        mysqli_free_result($result);
     }
-    $donated_person_detail=[];
-    mysqli_next_result($con);
-    $result=mysqli_store_result($con);
-    if(mysqli_num_rows($result)>0){
-        $donated_person_detail=mysqli_fetch_all($result,MYSQLI_ASSOC);
-    }
-    $content_detail=[];
-    mysqli_next_result($con);
-    $result = mysqli_store_result($con);
-    $content_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    mysqli_free_result($result);
-}
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>view my promises</title>
-    <link rel="stylesheet" href='/css_codes/view_my_event_individual_promise.css'>
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-    
-    <link href="/css_codes/bootstrap-toggle.css" rel="stylesheet">
-</head>
-<body>
-<script>
-    //btnPress(4);
-</script>
+    <head>
+        <title>view my promises</title>
+        <link rel="stylesheet" href='/css_codes/view_my_event_individual_promise.css'>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+        <link href="/css_codes/bootstrap-toggle.css" rel="stylesheet">
+    </head>
+    <body>
+        <script>
+            //btnPress(4);
+        </script>
 
+        <div id='promise_body'>
+            <div id="title">
+                <?php echo "My Promises" ?>
+            </div>
+            <table id='promise_table'>
+                <thead>
+                    <th colspan=1>Full name </th><th colspan=1>Promises</th><th colspan=1>Status</th><th colspan=1>Note</th>
+                </thead>
 
-
-<div id='promise_body'>
-<div id="title">
-    <?php echo "My Promises" ?>
-</div>
-    <table id='promise_table'>
-        <thead>
-        <th colspan=1>Full name </th>
-        <th colspan=1>Promises</th>
-        <th colspan=1>Note</th>
-        <th colspan=1>Status</th>
-        </thead>
-
-        <?php
-            foreach($promise_person_detail as $row_req){
-                $item_amount="";
-                $name=$row_req['first_name']." ".$row_req['last_name'];
-                $note=$row_req['note'];
-                $status=$row_req['pro_don'];
-                foreach($content_detail as $row_req1){
-                    if ($row_req1['don_id']==$row_req['id']){
-                    $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
+            <?php
+                foreach($person_detail as $row_req){
+                    $name=$row_req['first_name']." ".$row_req['last_name'];
+                    $note=$row_req['note'];
+                    $promise_array=[];
+                    $pending_array=[];
+                    foreach($content_detail as $row_req1){
+                        if ($row_req1['don_id']==$row_req['id']){
+                            $item_amount=$row_req1['item'].":".$row_req1['amount'];
+                            if ($row_req1['pro_don']=="promise"){
+                                array_push($promise_array,$item_amount);
+                            }else if ($row_req1['pro_don']=="pending"){
+                                array_push($pending_array,$item_amount);
+                            }
+                        }
+                    }
+                    $full_array=array_merge($pending_array,$promise_array);
+                    for($x=0; $x < count($full_array); $x++ ){
+                        $value=$full_array[$x];
+                        if(in_array($value,$pending_array)){$checked="checked='checked'";}else{$checked="";}
+                        if ($x==0){$name_data="<td rowspan=".count($full_array).">".$name."</td>";$note_data="<td rowspan=".count($full_array).">".$note."</td>";}else{$name_data=$note_data="";}
+                
+                        $to=$row_req['NIC_num'];
+                        echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$id."&to=".$to."\")'>
+                                ".$name_data."
+                            <td>".$value."</td>
+                            <td class='not_click'>
+                            <input type='checkbox'".$checked."data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
+                            </td>
+                            ".$note_data."
+                        </tr>";
                     }
                 }
-                if ($status=="promise"){
-                $to=$row_req['NIC_num'];
-                echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$id."&to=".$to."\")'>
-                            <td>".$name."</td><td>".$item_amount."</td>
-                            <td>".$note."</td>
-                            <td class='not_click'>
-                            <input type='checkbox' data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
-                            </td>
-                        </tr>";
-                }
-                else if ($status=="pending"){
-                    $to=$row_req['NIC_num'];
-                echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$id."&to=".$to."\")'>
-                            <td>".$name."</td><td>".$item_amount."</td>
-                            <td>".$note."</td>
-                            <td class='not_click'>
-                            <input type='checkbox' checked='checked' data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
-                            </td>
-                        </tr>";
-                }
-            }
-        ?>
-    </table>
-</div>
-<div id='donated_body'>
-<div id="title">
-    <?php echo "My Donations" ?>
-</div>
-    <table id='donated_table'>
-        <thead>
-        <th colspan=1>Full name </th>
-        <th colspan=1>Donations</th>
-        <th colspan=1>Note</th>
-        </thead>
+            ?>
+        </table>
+    </div>
+    <div id='donated_body'>
+        <div id="title">
+            <?php echo "My Donations" ?>
+        </div>
+        <table id='donated_table'>
+            <thead>
+                <th colspan=1>Full name </th><th colspan=1>Donations</th><th colspan=1>Note</th>
+            </thead>
 
         <?php
-            foreach($donated_person_detail as $row_req){
+            foreach($person_detail as $row_req){
                 $item_amount="";
                 $name=$row_req['first_name']." ".$row_req['last_name'];
                 $note=$row_req['note'];
                 foreach($content_detail as $row_req1){
                     if ($row_req1['don_id']==$row_req['id']){
-                    $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
+                        if ($row_req1['pro_don']=="donated"){
+                        $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
+                        }
                     }
                 }
+                if ($item_amount!=""){
                 echo "  <td>".$name."</td><td>".$item_amount."</td>
                             <td>".$note."</td>
-                        </tr>";
+                        </tr>";}
             }
         ?>
-    </table>
-</div>
+        </table>
+    </div>
 </body>
+
 <script>
     function edit_promise(url){
         var target = event.target ? event.target : event.srcElement;
--- event/view_promises_on_me.php
+++ event/view_promises_on_me.php
@@ -3,34 +3,21 @@
     require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
     $id=$_SESSION['user_nic'];
     $event_id = $_GET['event_id'];
-    $query="select * from event_".$event_id."_pro_don inner join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.by_person where to_person='".$id."' AND (pro_don='promise' OR pro_don='pending');
-    select * from event_".$event_id."_pro_don inner join organizations on organizations.org_id = event_".$event_id."_pro_don.by_org where to_person='".$id."' AND (pro_don='promise' OR pro_don='pending');
-    select * from event_".$event_id."_pro_don inner join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.by_person where to_person='".$id."' AND pro_don='donated' ;
-    select * from event_".$event_id."_pro_don inner join organizations on organizations.org_id = event_".$event_id."_pro_don.by_org where to_person='".$id."'AND pro_don='donated' ;
+    $query="select * from event_".$event_id."_pro_don inner join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.by_person where to_person='".$id."';
+    select * from event_".$event_id."_pro_don inner join organizations on organizations.org_id = event_".$event_id."_pro_don.by_org where to_person='".$id."';
     select * from event_".$event_id."_pro_don_content inner join event_".$event_id."_pro_don on event_".$event_id."_pro_don_content.don_id = event_".$event_id."_pro_don.id where to_person='".$id."';";
 
     if(mysqli_multi_query($con,$query)){
         $result=mysqli_store_result($con);
-        $promise_person_detail=[];
+        $person_detail=[];
         if(mysqli_num_rows($result)>0){
-            $promise_person_detail=mysqli_fetch_all($result,MYSQLI_ASSOC);
+            $person_detail=mysqli_fetch_all($result,MYSQLI_ASSOC);
         }
-        $promise_org_detail=[];
-        mysqli_next_result($con);
-        $result = mysqli_store_result($con);
-        $promise_org_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
-        mysqli_free_result($result);
 
-        $donated_person_detail=[];
+        $org_detail=[];
         mysqli_next_result($con);
         $result = mysqli_store_result($con);
-        $donated_person_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
-        mysqli_free_result($result);
-
-        $donated_org_detail=[];
-        mysqli_next_result($con);
-        $result = mysqli_store_result($con);
-        $donated_org_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        $org_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
         mysqli_free_result($result);
 
         $content_detail=[];
@@ -38,162 +25,116 @@
         $result = mysqli_store_result($con);
         $content_detail = mysqli_fetch_all($result,MYSQLI_ASSOC);
         mysqli_free_result($result);
+
+        $person_org_array=array_merge($person_detail,$org_detail);
     }
     ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>view promises on me</title>
-    <link rel="stylesheet" href='/css_codes/view_my_event_individual_promise.css'>
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <head>
+        <title>view promises on me</title>
+        <link rel="stylesheet" href='/css_codes/view_my_event_individual_promise.css'>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
     
-    <link href="/css_codes/bootstrap-toggle.css" rel="stylesheet">
-</head>
-<body>
-<script>
-    //btnPress(4);
-</script>
+        <link href="/css_codes/bootstrap-toggle.css" rel="stylesheet">
+    </head>
+    <body>
+        <script>
+            //btnPress(4);
+        </script>
 
-
-
-<div id='promise_body'>
-<div id="title">
-    <?php echo "Promises" ?>
-</div>
-    <table id='promise_table'>
-        <thead>
-        <th colspan=1>Full name </th>
-        <th colspan=1>Promises</th>
-        <th colspan=1>Note</th>
-        <th colspan=1>Status</th>
-        </thead>
-        <?php
-                    foreach($promise_person_detail as $row_req){
-                        $item_amount="";
-                        $name=$row_req['first_name'].$row_req['last_name'];
+        <div id='promise_body'>
+            <div id="title">
+                <?php echo "Promises" ?>
+            </div>
+            <table id='promise_table'>
+                <thead>
+                    <th colspan=1>Full name </th><th colspan=1>Promises</th><th colspan=1>Status</th><th colspan=1>Note</th>
+                </thead>
+                <?php
+                    foreach($person_org_array as $row_req){
+                        if (in_array($row_req,$person_detail)){
+                            $name=$row_req['first_name']." ".$row_req['last_name'];
+                            $by=$row_req['NIC_num'];
+                        }
+                        else{
+                            $name=$row_req['org_name'];
+                            $by=$row_req['org_id'];
+                        }
                         $note=$row_req['note'];
-                        $status=$row_req['pro_don'];
+                        $promise_array=[];
+                        $pending_array=[];
                         foreach($content_detail as $row_req1){
                             if ($row_req1['don_id']==$row_req['id']){
-                            $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
+                                $item_amount=$row_req1['item'].":".$row_req1['amount'];
+                                if ($row_req1['pro_don']=="promise"){
+                                    array_push($promise_array,$item_amount);
+                                }else if ($row_req1['pro_don']=="pending"){
+                                    array_push($pending_array,$item_amount);
+                                }
                             }
                         }
-                        if ($status=="promise"){
-                            $to=$row_req['NIC_num'];
-                            echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$id."&to=".$to."\")'>
-                                        <td>".$name."</td><td>".$item_amount."</td>
-                                        <td>".$note."</td>
-                                        <td class='not_click'>
-                                        <input type='checkbox' data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
-                                        </td>
-                                    </tr>";
-                            }
-                            else if ($status=="pending"){
-                                $to=$row_req['NIC_num'];
-                            echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$id."&to=".$to."\")'>
-                                        <td>".$name."</td><td>".$item_amount."</td>
-                                        <td>".$note."</td>
-                                        <td class='not_click'>
-                                        <input type='checkbox' checked='checked' data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
-                                        </td>
-                                    </tr>";
-                            }
+                        $full_array=array_merge($pending_array,$promise_array);
+                        for($x=0; $x < count($full_array); $x++ ){
+                            $value=$full_array[$x];
+                            if(in_array($value,$pending_array)){$checked="checked='checked'";}else{$checked="";}
+                            if ($x==0){$name_data="<td rowspan=".count($full_array).">".$name."</td>";$note_data="<td rowspan=".count($full_array).">".$note."</td>";}else{$name_data=$note_data="";}
+                         
+                        echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$by."&to=".$id."\")'>
+                            ".$name_data."
+                            <td>".$value."</td>
+                            <td class='not_click'>
+                            <input type='checkbox'".$checked."data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
+                            </td>
+                            ".$note_data."
+                        </tr>";
+                
+                
                         }
-                    
-
-
-                    foreach($promise_org_detail as $row_req){
-                        $item_amount="";
-                        $name=$row_req['org_name'];
-                        $note=$row_req['note'];
-                        $status=$row_req['pro_don'];
-                        foreach($content_detail as $row_req1){
-                            if ($row_req1['don_id']==$row_req['id']){
-                            $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
-                            }
-                        }
-                        if ($status=="promise"){
-                            $to=$id;
-                            $by=$row_req['id'];
-                            echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$by."&to=".$to."\")'>
-                                        <td>".$name."</td><td>".$item_amount."</td>
-                                        <td>".$note."</td>
-                                        <td class='not_click'>
-                                        <input type='checkbox' data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
-                                        </td>
-                                    </tr>";
-                            }
-                            else if ($status=="pending"){
-                                $to=$id;
-                                $by=$row_req['id'];
-                            echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$by."&to=".$to."\")'>
-                                        <td>".$name."</td><td>".$item_amount."</td>
-                                        <td>".$note."</td>
-                                        <td class='not_click'>
-                                        <input type='checkbox' checked='checked' data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange=''>
-                                        </td>
-                                    </tr>";
-                            }
-                        }
-                    
-                    
+                    }    
                 ?>
-    </table>
-</div>
-<div id='donated_body'>
-<div id="title">
-    <?php echo "Donations" ?>
-</div>
-    <table id='donated_table'>
-        <thead>
-        <th colspan=1>Full name </th>
-        <th colspan=1>Donations</th>
-        <th colspan=1>Note</th>
-        </thead>
-
-        <?php
-                    foreach($donated_person_detail as $row_req){
+            </table>
+        </div>
+        <div id='donated_body'>
+            <div id="title">
+                <?php echo "Donations" ?>
+            </div>
+            <table id='donated_table'>
+                <thead>
+                    <th colspan=1>Full name </th><th colspan=1>Donations</th><th colspan=1>Note</th>
+                </thead>
+                <?php
+                    foreach($person_org_array as $row_req){
+                        if (in_array($row_req,$person_detail)){$name=$row_req['first_name']." ".$row_req['last_name'];}
+                        else{$name=$row_req['org_name'];}
                         $item_amount="";
-                        $name=$row_req['first_name']." ".$row_req['last_name'];
                         $note=$row_req['note'];
                         foreach($content_detail as $row_req1){
                             if ($row_req1['don_id']==$row_req['id']){
-                            $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
+                                if ($row_req1['pro_don']=="donated"){
+                                $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
+                            }
                             }
                         }
-                        echo "<tr>
-                    <td>".$name."</td><td>".$item_amount."</td><td>".$note."</td>
-                    </tr>";
-                    }
-                    foreach($donated_org_detail as $row_req){
-                        $item_amount="";
-                        $name=$row_req['org_name'];
-                        $note=$row_req['note'];
-                        foreach($content_detail as $row_req1){
-                            if ($row_req1['don_id']==$row_req['id']){
-                            $item_amount=$item_amount.$row_req1['item'].":".$row_req1['amount']."<br>";
-                            }
-                        }
-                        echo "<tr>
-                    <td>".$name."</td><td>".$item_amount."</td><td>".$note."</td>
-                    </tr>";
-                    }
-                    
+                        if ($item_amount!=""){
+                        echo "  <td>".$name."</td><td>".$item_amount."</td>
+                                    <td>".$note."</td>
+                                </tr>";}
+                    }   
                 ?>
-    </table>
-</div>
-</body>
-<script>
-    function edit_promise(url){
-        var target = event.target ? event.target : event.srcElement;
-        if(!'not_click toggle btn btn-warning off toggle-group btn btn-success toggle-on btn btn-warning active toggle-off toggle-handle btn btn-default'.includes(target.className)){
-            window.location=url;
-        }else{
-            
+            </table>
+        </div>
+    </body>
+    <script>
+        function edit_promise(url){
+            var target = event.target ? event.target : event.srcElement;
+            if(!'not_click toggle btn btn-warning off toggle-group btn btn-success toggle-on btn btn-warning active toggle-off toggle-handle btn btn-default'.includes(target.className)){
+                window.location=url;
+            }else{
+                
+            }
         }
-    }
-</script>
-
+    </script>
 </html>
