9ed38218b3b14d0f2550576cb215a469829a2f0b
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-07-20 14:44:59 +0530

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,21 @@
 <?php
-    require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
-    require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
+    require_once $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="/css_codes/header.css">
-        <link rel='stylesheet' type='text/css' href='/css_codes/events.css'>
         <link rel='stylesheet' type='text/css' href='/css_codes/side_nav.css'>
+        <link rel="shortcut icon" href="/common/logo.jpg" type="image/x-icon" /> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+        <meta charset="UTF-8">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+        <meta name="author" content="John Doe">
+        <script src='/js/font_awesome.js' defer></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
 
     <body>
@@ -36,14 +41,23 @@
             </div>
             <div>
                 <div id="menubar">
-                    <a href="/home_page.php"> <button type="submit" class="menubar_buttons" id='menu_bar_btn_1'>Home</button> </a>
-                    <a href="/govermentpost.php"> <button type="submit" class="menubar_buttons" id='menu_bar_btn_2'>Goverment posts</button> </a>
-                    <a href="/publicpost"><button class="menubar_buttons" id='menu_bar_btn_3'>Public posts</button></a>
-                    <div class='menubar_button_container'><button class="menubar_buttons" id='menu_bar_btn_4' onclick='showevent(this)'>Events</button><div id=event_container></div></div>
-                    <div class='menubar_button_container'><button class="menubar_buttons" id='menu_bar_btn_5' onclick="show_notification(this)">Notifications</button><div id=notification_container></div></div>
-                    <div class='menubar_button_container'><button type="submit" class="menubar_buttons" name='menubar_buttons' id='menu_bar_btn_6' value=6 onclick='show_org(this)'>Organizations</button><div id=menubar_org_container></div></div>
-                    <a href="/fundraising"><button class="menubar_buttons" id=menu_bar_btn_7>Fundraisings</button></a>
-                    <a href=""> <button type="submit" class="menubar_buttons" id='menu_bar_btn_8'>About</button> </a>
+                    <!-- <a href="/home_page.php"> <div class="menubar_buttons" id='menu_bar_btn_1'>Home</div> </a> -->
+                    <a href="/govermentpost.php" class="menubar_buttons_cont"> <div type="submit" class="menubar_buttons"><div class="menu_icon_tooltip"><i class='fas fa-university menu_icon menu_bar_btn_2'></i><span class="menu_icon_tooltiptext">Goverment post</span></div></div> </a>
+                    <a href="/publicpost" class="menubar_buttons_cont"><div class="menubar_buttons" ><div class="menu_icon_tooltip"><i class='fas fa-book-reader menu_icon menu_bar_btn_3'></i><span class="menu_icon_tooltiptext">Public post</span></div></div></a>
+                    <div class='menubar_button_container menubar_buttons_cont'><div class="menubar_buttons" onclick='showevent(this)'><div class="menu_icon_tooltip"><i class='far fa-calendar-alt menu_icon menu_bar_btn_4'></i><span class="menu_icon_tooltiptext">Events</span></div></div><div id=event_container></div></div>
+                    <div class='menubar_button_container menubar_buttons_cont'><div type="submit" class="menubar_buttons" name='menubar_buttons'  value=6 onclick='show_org(this)'><div class="menu_icon_tooltip"><i class='fas fa-users menu_icon menu_bar_btn_6'></i><span class="menu_icon_tooltiptext">Organization</span></div></div><div id=menubar_org_container></div></div>
+                    <a href="/fundraising" class="menubar_buttons_cont"><div class="menubar_buttons" ><div class="menu_icon_tooltip"><i class='fas fa-hand-holding-heart menu_icon menu_bar_btn_7'></i><span class="menu_icon_tooltiptext">Fundraising</span></div></div></a>
+                    <a href="/chat.php" class="menubar_buttons_cont"> <div type="submit" class="menubar_buttons" ><div class="menu_icon_tooltip"><i class='fas fa-comments menu_icon menu_bar_btn_8'></i><span class="menu_icon_tooltiptext">Chat</span></div></div> </a>
+                    <div class='menubar_button_container menubar_buttons_cont' ><div class="menubar_buttons"  onclick="show_notification(this)"><div class="menu_icon_tooltip"><i class='fas fa-bell menu_icon menu_bar_btn_5'></i><span class="menu_icon_tooltiptext">Notification</span></div></div><div id=notification_container></div></div>
+                    <div class='menubar_button_container dropdown_cont'><div  class="menubar_buttons dropdown_btn" onclick='show_dropdown(this)'><div class="menu_icon_tooltip">
+                        <i class="fa fa-caret-down" style="font-size:30px"></i>
+                    <span class="menu_icon_tooltiptext">More</span></div></div>
+                    <div id=dropdown_container class='dropdown_container'> 
+                        <a class="header_drop_down" href='Apple'>Apple</a>
+                        <a class="header_drop_down" href="Banana">Orane</a>
+                        <a class="header_drop_down" href='Orange'>Banana</a>
+                    </div></div>
+                    <!-- <a href=""> <button type="submit" class="menubar_buttons" id='menu_bar_btn_8'>About</button> </a> -->
                 </div>
             </div>
 
