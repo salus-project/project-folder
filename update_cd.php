c725967e5382adbd8fde1f6508647edbc2c39d5e
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-07-22 10:10:01 +0530

    Add files via upload

--- update_cd.php
+++ update_cd.php
@@ -6,58 +6,66 @@ require 'edit_profile.php';
 <html>
     <head>
         <title> Update</title>
-        <link rel="stylesheet" href="css_codes/style.css">
+        <link rel="stylesheet" href="/css_codes/edit_profile.css">
     </head>
 
     <body>
 
     <script> btnPress(1) </script>
 
-        <center>
-            <h1> Update my Info </h1>
-            
-            <div class="div1">
-            <form  class="form_box" action="update_cd.php" method="POST">
-
-                <label class="label">First Name </label><br>
-                <input name = "first_name" type="text" class="input_box" value="<?php echo $_SESSION['first_name']; ?>" required /><br>
+            <div class="edit_prof_form_box">
+            <form action="update_cd.php" method="POST">
+                <div class="head_edit_prof_form"> <div class="head_edit_prof_form_det"><?php echo $_SESSION['first_name']." ".$_SESSION['last_name'];   ?>
+                </div></div>
+                <div class="body_edit_prof_form">
                 
-                <label class="label">Last Name </label><br>
-                <input name = "last_name" type="text" class="input_box" value="<?php echo $_SESSION['last_name']; ?>" required/><br>
-
-                <label class="label"> Gender </label><br>
-                <select  name="gender" type="text" class="input_box" value="<?php echo $_SESSION['gender']; ?>" required/><br>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">First Name </label><br>
+                <input name = "first_name" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['first_name']; ?>" required /><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">Last Name </label><br>
+                <input name = "last_name" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['last_name']; ?>" required/><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label"> Gender </label><br>
+                <select  name="gender" type="text" class="edit_prof_input_box" <?php echo $_SESSION['gender']; ?> required/><br>
+                <?php if ($_SESSION['gender']=="male"){ echo "<option selected value=\"male\">Male</option>";} else{echo "<option  value=\"male\">Male</option>";} ?>
+                <?php if ($_SESSION['gender']=="female"){ echo "<option selected value=\"female\">Female</option>";} else{echo "<option  value=\"female\">Female</option>";} ?>
+                <?php if ($_SESSION['gender']=="other"){ echo "<option selected value=\"other\">Male</option>";} else{echo "<option  value=\"other\">Other</option>";} ?>
                 </select>
-
-                <label class="label">District </label><br>
-                <input name = "district" type="text" class="input_box" value="<?php echo $_SESSION['district']; ?>" required/><br>
-
-                <label class="label">Occupation </label><br>
-                <input name = "occupation" type="text" class="input_box" value="<?php echo $_SESSION['Occupation']; ?>"required/><br>
-
-                <label class="label">Address </label><br>
-                <input name = "address" type="text" class="input_box" value="<?php echo $_SESSION['address']; ?>"required/><br>
-
-                <label class="label">Email </label><br>
-                <input name = "email" type="text" class="input_box" value="<?php echo $_SESSION['email']; ?>"required/><br>
-
-                <label class="label">Phone Number </label><br>
-                <input name = "phone_num" type="text" class="input_box" value="<?php echo $_SESSION['phone_num']; ?>"required/><br>
-                
-                <label class="label">Password </label><br>
-                <input name="password" type="password" class="input_box" required/><br>
-                
-                <input name="update_button" type="submit"  value="Update"  class="login_button"><br>
-                
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">District </label><br>
+                <input name = "district" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['district']; ?>" required/><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">Occupation </label><br>
+                <input name = "occupation" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['Occupation']; ?>"required/><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">Address </label><br>
+                <input name = "address" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['address']; ?>"required/><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">Email </label><br>
+                <input name = "email" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['email']; ?>"required/><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">Phone Number </label><br>
+                <input name = "phone_num" type="text" class="edit_prof_input_box" value="<?php echo $_SESSION['phone_num']; ?>"required/><br>
+                </div>
+                <div class="edit_prof_grp">
+                <label class="edit_form_label">Password </label><br>
+                <input name="password" type="password" class="edit_prof_input_box" required/><br>
+                </div>
+                </div>
+                <div class="foot_edit_prof_form">
+                    <button name="update_button" type="submit"  value="Update"  class="edit_form_submit_button">Update</button>
+                </div>
                 
             </form>
-
             
-            </div>
-        <center>
-        
+            </div> 
     </body>
 </html>
