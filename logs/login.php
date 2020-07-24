bb594721c41bb3e708956bb1dd53814b2ff10a78
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-07-24 11:29:00 +0530

    Add files via upload

--- logs/login.php
+++ logs/login.php
@@ -1,6 +1,37 @@
 <?php
-session_start();
-require_once $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        
+        $nic_num=$_POST['nic_num'];
+        $password=$_POST['password'];
+        $query="select * from civilian_detail where NIC_num='$nic_num' AND password='$password'";
+        //$query_run = mysqli_query($con,$query);
+        $result=$con->query($query);
+        if($result->num_rows>0){
+            
+            while($row=$result->fetch_assoc()){
+                //$_SESSION['']=$row[''];
+                $_SESSION['user_nic'] = $row["NIC_num"];
+                $_SESSION['first_name']=$row["first_name"];
+                $_SESSION['last_name']=$row['last_name'];
+                $_SESSION['side_nav']=1;
+            }
+
+            if($_POST['location']=='http://localhost/logs/login.php' || $_POST['location']==''){
+                header("Location:/govermentpost.php");
+            }else{
+                //header('location:'.str_replace(PHP_EOL, '', $_POST['location']));
+                header('location:'.$_POST['location']);
+            }
+            
+
+        }else{
+            echo '<script type="text/javascript">';
+            echo 'alert("Invalid NIC num or password")';
+            echo '</script>';
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -26,47 +57,8 @@ require_once $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
                 <input name="password" type="password" class="input_box" placeholder="Enter your password" required/><br>
                 <input name="login_button" type="submit" class="login_button" value="Login"/><br>
                 <a href="https://www.google.com">Don't have an account yet</a>
-                
-
+                <input type='hidden' name='location' value='<?php echo isset($_SERVER["HTTP_REFERER"])?$_SERVER["HTTP_REFERER"]:''; ?>'>
             </form>
-            <?php
-                if($_SERVER["REQUEST_METHOD"]=="POST"){
-                    
-                    $nic_num=$_POST['nic_num'];
-                    $password=$_POST['password'];
-                    $query="select * from civilian_detail where NIC_num='$nic_num' AND password='$password'";
-                    //$query_run = mysqli_query($con,$query);
-                    $result=$con->query($query);
-                    if($result->num_rows>0){
-                        
-                        while($row=$result->fetch_assoc()){
-                            //$_SESSION['']=$row[''];
-                            $_SESSION['user_nic'] = $row["NIC_num"];
-                            $_SESSION['first_name']=$row["first_name"];
-                            $_SESSION['last_name']=$row['last_name'];
-                            $_SESSION['gender']=$row['gender'];
-                            $_SESSION['district']=$row['district'];
-                            $_SESSION['Occupation']=$row['Occupation'];
-                            $_SESSION['address']=$row['address'];
-                            $_SESSION['email']=$row['email'];
-                            $_SESSION['phone_num']=$row['phone_num'];
-                        }
-                        
-                        
-                        header('location:/home_page.php');
-
-                    }else{
-                        echo '<script type="text/javascript">';
-                        echo 'alert("Invalid NIC num or password")';
-                        echo '</script>';
-                    }
-                }
-
-            ?>
-            
         </div>
-
     </body>
-
-
 </html>
--- logs/logout.php
+++ logs/logout.php
@@ -1,9 +1,6 @@
 <?php
     session_start();
-    if(isset($_POST['logout'])){
-        echo 'logout';
         session_unset();
         session_destroy();
         $_SESSION = [];
-        header('location:/logs/login.php');
-    }
+        header('location: /logs/login.php');
