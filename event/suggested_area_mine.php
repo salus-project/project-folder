e60ff2571ebf0e54df8405f74d0b77ec36d15d3f
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-07-25 10:36:46 +0530

    Add files via upload

--- event/suggested_area_mine.php
+++ event/suggested_area_mine.php
@@ -5,13 +5,13 @@
     $Nic_num= $_SESSION['user_nic'];
 ?>
 
-<html>
-    <head>
-        <title>My suggested areas</title>
-        <link rel="stylesheet" href='/css_codes/suggested_area_mine.css'>
-    </head>
-    <body>
-    <div class="my_suggest_cont">
+
+<head>
+    <title>My suggested areas</title>
+    <link rel="stylesheet" href='/css_codes/suggested_area_mine.css'>
+</head>
+<body>
+<div class="my_suggest_cont">
     <div class='my_suggest_table_cont'>
         <table class='table_suggested_area'>
             <tr class='table_suggested_area_tr'>
@@ -24,7 +24,7 @@
             $result=$con->query($query);
             if($result->num_rows>0){
                 while($row=$result->fetch_assoc()){
-                    echo "<tr class='table_suggested_area_tr'>";
+                    echo "<tr class='table_suggested_area_tr' data-areadata='".json_encode($row)."' onclick=add_place_preview(this)>";
                         echo "<td class='table_suggested_area_td'>".ucfirst(explode("_",$row['type'])[0])." area</th>";
                         echo "<td  class='table_suggested_area_td'>".$row['detail']."</th>";
                         echo "<td  class='table_suggested_area_td'><a href='suggested_area_all_delete.php?event_id=".$_GET['event_id']."&location_id=".$row['id']."' class='my_suggest_del'>Delete</a></th>";
@@ -35,9 +35,22 @@
         </table>
     </div>
     <div class='my_suggest_map_cont'>
-    Map Map Map
+        <?php require $_SERVER['DOCUMENT_ROOT']."/common/map/map.html"; ?>
     </div>
-    <div>
-        <?php require $_SERVER['DOCUMENT_ROOT']."/organization/org_footer.php"; ?>
-    </body>
-</html>
+</div>
+    <?php require $_SERVER['DOCUMENT_ROOT']."/organization/org_footer.php"; ?>
+<script>
+    var myGeo = new EventGeo();
+    myGeo.markPlace(areaGeoJson, 'danger', 'Affected area',  true);
+
+    var remove=false;
+
+    function add_place_preview(ele){
+        if(remove){
+            myGeo.remove_last_place_circle();
+        }else{
+            remove=true;
+        }
+        create_place(JSON.parse(ele.dataset.areadata),true);
+    }
+</script>
