174dc3d983fe4d09eb8a4773e7a2ef127ce4fa23
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-08-01 19:09:06 +0530

    Add files via upload

--- event/index.php
+++ event/index.php
@@ -7,21 +7,21 @@
 </div>
 <div id=event_body>
     <div id='table_caontainer'>
-        <table id=view_event_table>
-            <thead>
-                <th class='head' colspan=2>
-                    Event Detail
-                </th>
-            </thead>
-            <?php
-                echo "<tr><td class='view_event_td'>" . ucfirst('Name') . "</td><td id=column2>" . ucfirst($result['name']) . "</td></tr>";
-                echo "<tr><td class='view_event_td'>" . ucfirst('Affected districts') . "</td><td id=column2>" . ucfirst($result['affected_districts']) . "</td></tr>";
-                echo "<tr><td class='view_event_td'>" . ucfirst('Start date') . "</td><td id=column2>" . ucfirst($result['start_date']) . "</td></tr>";
-                echo "<tr><td class='view_event_td'>" . ucfirst('End date') . "</td><td id=column2>" . ucfirst($result['end_date']) . "</td></tr>";
-                echo "<tr><td class='view_event_td'>" . ucfirst('Status') . "</td><td id=column2>" . ucfirst($result['status']) . "</td></tr>";
-                echo "<tr><td class='view_event_td'>" . ucfirst('Deaths and damages') . "</td><td id=column2>" . ucfirst($result['detail']) . "</td></tr>";
-            ?>
-        </table>
+        <div class='head' colspan=2>
+            Event Detail
+        </div>
+        <div class='event_con'>
+            <table id=view_event_table>           
+                <?php
+                    echo "<tr><td class='view_event_td'>" . ucfirst('Name') . "</td><td id=column2>" . ucfirst($result['name']) . "</td></tr>";
+                    echo "<tr><td class='view_event_td'>" . ucfirst('Affected districts') . "</td><td id=column2>" . ucfirst($result['affected_districts']) . "</td></tr>";
+                    echo "<tr><td class='view_event_td'>" . ucfirst('Start date') . "</td><td id=column2>" . ucfirst($result['start_date']) . "</td></tr>";
+                    echo "<tr><td class='view_event_td'>" . ucfirst('End date') . "</td><td id=column2>" . ucfirst($result['end_date']) . "</td></tr>";
+                    echo "<tr><td class='view_event_td'>" . ucfirst('Status') . "</td><td id=column2>" . ucfirst($result['status']) . "</td></tr>";
+                    echo "<tr><td class='view_event_td'>" . ucfirst('Deaths and damages') . "</td><td id=column2>" . ucfirst($result['detail']) . "</td></tr>";
+                ?>
+            </table>
+        </div>
     </div>
     <div id=pictures>
         <h3 class='head'>Photos</h3>
@@ -54,55 +54,54 @@
 
 <div id=social_events>
     <div id=help_requested>
-        <table id=view_event_table>
-        <thead>
-            <th class='head' colspan=2>
+        <div class='head' colspan=2>
                 Help requested people
-            </th>
-        </thead>
-        <?php
-            foreach($help_requested as $row_req){
-                $nic_num=$row_req["NIC_num"];
-                    $full_name=$row_req["first_name"].' '.$row_req["last_name"];
-                    echo "<tr>";
-                    echo    "<td class='view_event_td'>";
-                    echo         "<a class='person_a' href='/event/requester.php?event_id=".$_GET['event_id']."&nic=".$nic_num."'>".$full_name."</a>";
-                    echo    "</td>";
-                    echo "</tr>";
-                
-            }
-        ?>
-        </table>
+        </div>
+        <div class='table_con'>
+            <table id=view_event_table>     
+            <?php
+                foreach($help_requested as $row_req){
+                    $nic_num=$row_req["NIC_num"];
+                        $full_name=$row_req["first_name"].' '.$row_req["last_name"];
+                        echo "<tr>";
+                        echo    "<td class='view_event_td'>";
+                        echo         "<a class='person_a' href='/event/requester.php?event_id=".$_GET['event_id']."&nic=".$nic_num."'>".$full_name."</a>";
+                        echo    "</td>";
+                        echo "</tr>";
+                    
+                }
+            ?>
+            </table>
+        </div>
     </div>
     <div id=affected>
-        <table id=view_event_table>
-        <thead>
-            <th class='head' colspan=2>
+        <div class='head' colspan=2>
                 Volunteers detail
-            </th>
-        </thead>
-        <?php
-            foreach($volunteers as $row_req){
-                $nic_num=$row_req["NIC_num"];
-                    $full_name=$row_req["first_name"].' '.$row_req["last_name"];
-                    echo "<tr>";
-                    echo    "<td class='view_event_td'>";
-                    echo                "<a class='person_a' href='/event/volunteer.php?event_id=".$_GET['event_id']."&nic=".$nic_num."'>".$full_name."</a>";
-                    echo    "</td>";
-                    echo "</tr>";
-                
-            }
-        ?>
-        </table>
+        </div>
+        <div class='table_con'>
+            <table id=view_event_table>
+            <?php
+                foreach($volunteers as $row_req){
+                    $nic_num=$row_req["NIC_num"];
+                        $full_name=$row_req["first_name"].' '.$row_req["last_name"];
+                        echo "<tr>";
+                        echo    "<td class='view_event_td'>";
+                        echo                "<a class='person_a' href='/event/volunteer.php?event_id=".$_GET['event_id']."&nic=".$nic_num."'>".$full_name."</a>";
+                        echo    "</td>";
+                        echo "</tr>";
+                    
+                }
+            ?>
+            </table>
+        </div>
     </div>
     <div id=organizations>
-        <table id=view_event_table>
-            <thead>
-                <th class='head' colspan=2>
-                    Volunteers detail
-                </th>
-            </thead>
-                <?php
+        <div class='head' colspan=2>
+            Organizations on action
+        </div>
+        <div class='table_con'>
+            <table id=view_event_table>
+             <?php
                 foreach($orgs as $row){
                     echo "<tr>";
                     echo    "<td class='view_event_td'>";         
@@ -111,7 +110,8 @@
                     echo "</tr>";
                 }
                 ?>
-        </table>
+            </table>
+        </div>
     </div>
 </div>
 <div id=news_field>
