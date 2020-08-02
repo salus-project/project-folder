1a022b51a3d1af7d1f64e63ef32e497eeb1ece27
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-08-02 12:07:55 +0530

    Add files via upload

--- confi/db_confi.php
+++ confi/db_confi.php
@@ -1,37 +1,126 @@
 <?php
     //error_reporting(0);
-    try{
-        $con = mysqli_connect("remotemysql.com","kfm2yvoF5R","4vkzHfeBh6") or die('Unable to connect')/*header('location:'.$_SERVER['DOCUMENT_ROOT'].'/confi/error.html')*/;
-        mysqli_select_db($con,"kfm2yvoF5R");
+    class ConnectDb
+    {
+        private static $instance = null;
+        private $con;
 
-        $org_DB = mysqli_connect("remotemysql.com","LvFAfm4fFA","JGhOtcM4ez") or die("Unable to connect");
-        mysqli_select_db($org_DB,"LvFAfm4fFA");
+        private $host = "remotemysql.com";
+        private $user = "kfm2yvoF5R";
+        private $pass = "4vkzHfeBh6";
+        private $name = "kfm2yvoF5R";
 
-        $notification_DB = mysqli_connect("remotemysql.com","LlvvpTpgPp","t6rKpohvCB") or die("Unable to connect");
-        mysqli_select_db($notification_DB,"LlvvpTpgPp");
-    }catch(Exception $e){
-        header('location:'.$_SERVER['DOCUMENT_ROOT'].'/confi/error.html');
+        private function __construct()
+        {
+            $this->con = mysqli_connect($this->host, $this->user, $this->pass) or header('location:/confi/error.php');
+            mysqli_select_db($this->con, $this->name);
+        }
+
+        public static function getConnection()
+        {
+            if (!self::$instance) {
+                self::$instance = new ConnectDb();
+            }
+            return self::$instance->con;
+        }
+
+        public static function close_con(){
+            if (self::$instance) {
+                self::$instance->con->close();
+            }
+        }
     }
 
-    function shutdown(){
-        global $con,$org_DB,$notification_DB;
+    class NotificationDb
+    {
+        private static $instance = null;
+        private $con;
+
+        private $host = "remotemysql.com";
+        private $user = "LlvvpTpgPp";
+        private $pass = "t6rKpohvCB";
+        private $name = "LlvvpTpgPp";
+
+        private function __construct()
+        {
+            $this->con = mysqli_connect($this->host, $this->user, $this->pass) or header('location:/confi/error.php');
+            mysqli_select_db($this->con, $this->name);
+        }
+
+        public static function getConnection()
+        {
+            if (!self::$instance) {
+                self::$instance = new NotificationDb();
+            }
+            return self::$instance->con;
+        }
+
+        public static function close_con(){
+            if (self::$instance) {
+                self::$instance->con->close();
+            }
+        }
+    }
+
+    class OrgDb
+    {
+        private static $instance = null;
+        private $con;
+
+        private $host = "remotemysql.com";
+        private $user = "LvFAfm4fFA";
+        private $pass = "JGhOtcM4ez";
+        private $name = "LvFAfm4fFA";
+
+        private function __construct()
+        {
+            $this->con = mysqli_connect($this->host, $this->user, $this->pass) or header('location:/confi/error.php');
+            mysqli_select_db($this->con, $this->name);
+        }
+
+        public static function getConnection()
+        {
+            if (!self::$instance) {
+                self::$instance = new OrgDb();
+            }
+            return self::$instance->con;
+        }
+
+        public static function close_con(){
+            if (self::$instance) {
+                self::$instance->con->close();
+            }
+        }
+    }
+
+    $con = ConnectDB::getConnection();
+
+
+    function shutdown()
+    {
+        /*global $con, $org_DB, $notification_DB;
+        
         $con->close();
         $org_DB->close();
-        $notification_DB->close();
+        $notification_DB->close();*/
+
+        ConnectDB::close_con();
+        NotificationDb::close_con();
+        OrgDb::close_con();
 
         ob_end_flush();
     }
     register_shutdown_function('shutdown');
 
-    function filt_inp($data){
+    function filt_inp($data)
+    {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
     }
 
-    function ready_input($input){
+    function ready_input($input)
+    {
         return strtolower(trim($input));
     }
-    
-?>
