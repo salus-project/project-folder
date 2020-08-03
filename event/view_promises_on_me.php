2224bbd37a426e7ddb34ada03e15d4715d861392
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-08-03 11:20:54 +0530

    Add files via upload

--- event/view_promises_on_me.php
+++ event/view_promises_on_me.php
@@ -67,7 +67,7 @@
                                 $item_amount=$row_req1['item'].":".$row_req1['amount'];
                                 if (($row_req1['pro_don']=="promise") && ($row_req1['pro_don']=="pending")){
                                     array_push($promise_array,$item_amount);
-                                }else if ($row_req1['pro_don']=="helped"){
+                                }else if ($row_req1['pro_don']=="donated"){
                                     array_push($pending_array,$item_amount);
                                 }
                                 array_push($full_array,$item_amount);
@@ -155,7 +155,7 @@
         }
         function toggleFn(ele,event,id){
         if (ele.checked){
-        var c_status='helped';
+        var c_status='donated';
         }else{
         var c_status='pending';
         }
