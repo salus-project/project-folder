3623b7de11c906ace3d822f32bc167355469c130
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-08-07 00:51:47 +0530

    Add files via upload

--- event/help/index_php.php
+++ event/help/index_php.php
@@ -1,4 +1,7 @@
 <?php
+    ob_start();
+    ignore_user_abort();
+
     require $_SERVER['DOCUMENT_ROOT']."/confi/db_confi.php";
     require $_SERVER['DOCUMENT_ROOT']."/confi/verify.php";
  
@@ -11,9 +14,11 @@
             if($by=="your_self"){
                 $pri_query="DELETE from event_".$event_id."_pro_don_content WHERE don_id=(select id from event_".$event_id."_pro_don WHERE by_person='".$by_person."' and to_person='".$to."');
                 DELETE from event_".$event_id."_pro_don WHERE by_person='".$by_person."' and to_person='".$to."';";
+                $msg=" cancel his/her promise to help you";
             }else{
                 $pri_query="DELETE from event_".$event_id."_pro_don_content WHERE don_id=(select id from event_".$event_id."_pro_don WHERE by_org='".$by."' and to_person='".$to."');
                 DELETE from event_".$event_id."_pro_don WHERE by_org='".$by."' and to_person='".$to."';";
+                $msg=" cancel their promise to help you";
             }
         }else{
             $item = array_filter($_POST['item']);
@@ -28,6 +33,7 @@
                 foreach( $del_detail as $row_del){
                     $pri_query.= "delete from event_".$event_id."_pro_don_content where id=$row_del;";
                 }
+                $msg=" edited their promised to help you";
                 $pri_query.="update event_".$event_id."_pro_don set note='".$note."' where id={$_POST['entry_update_id']};";
             
                 for($x=0 ; $x < count($item) ; $x++){
@@ -51,6 +57,7 @@
                     $by_person='';
                     $by_org=$by;
                 }
+                $msg=" promised to help you";
                 $pri_query = "insert into event_".$event_id."_pro_don (by_org, by_person, to_person, note) values ($by_org, '$by_person', '$to', '".$note."');";
                 if(count($item)>0){
                     $querry_arr = array();
@@ -63,9 +70,28 @@
                 }
             }
         }
-      
-        if(mysqli_multi_query($con, $pri_query)){
+        $sql="SELECT org_name FROM `organizations` WHERE org_id=$by;";
+        if(mysqli_multi_query($con,$sql.$pri_query)){
+            $size = ob_get_length();
+            header("Content-Encoding: none");
+            header("Content-Length: {$size}");
+            header("location:".$_SERVER['HTTP_REFERER']);
+            header("Connection: close");
+
             header('location:'.$_SERVER['HTTP_REFERER']);
+
+            ob_end_flush();
+            ob_flush();
+            flush();
+
+            $sql_res=mysqli_store_result($con);
+            $result1=$sql_res->fetch_row();
+            mysqli_free_result($sql_res);
+
+            require $_SERVER['DOCUMENT_ROOT']."/notification/notification_sender.php";
+            $sender = new Notification_sender($to,$result1[0].$msg,"/event/view_my_individual_promises.php?event_id=".$event_id,true);
+            $sender->send();
+            
         }else{
             //header("location:/event/help?event_id=".$event_id."&by=".$by."&to=".$to);
             echo "not sucess";
