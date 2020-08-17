6ab36bb6339b9f46de832a494df7e51bad81c8b1
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-08-17 12:49:33 +0530

    Add files via upload

--- fundraising/edit_img/index.php
+++ fundraising/edit_img/index.php
@@ -10,10 +10,10 @@
     $fundraising_name = $result['name'];
     $imgs = array_filter(explode(',', $result['img']));
 ?>
-
+<div class='fund_head' colspan=2><?php echo $fundraising_name ?></div>
 <div class="img_cont">
     <div class='fund_image_conatainer'>
-        <h3>Profile Image</h3>
+        <div class='img_type'>Profile Image</div>
         <div class="fund_image prim">
             <img src="http://d-c-a.000webhostapp.com/Fundraising/<?php echo $id ?>.jpg" alt="Opps..." class="fund_pic">
         </div>
@@ -26,12 +26,10 @@
         </form>
         <button id='edit_profile_btn' onclick="this.previousElementSibling.firstElementChild.click()"><i class="fa fa-camera" aria-hidden="true"></i>Change</button>
     </div>
-
-    <div class='fund_image_secondary_container'>
-    <h3>Secondary Images</h3>
         <?php
         foreach ($imgs as $img) {?>
-            <div class="fund_image_conatainer">
+            <div class="fund_image_conatainer"> 
+                <div class='img_type'>Secondary Images</div>
                 <div class="fund_image seco">
                     <img src="http://d-c-a.000webhostapp.com/Fundraising/secondary/<?php echo $img ?>.jpg" alt="Opps..." class="fund_pic">
                 </div>
@@ -45,14 +43,12 @@
             </div>
         <?php }
         ?>
-    </div>
     <div class='new_photo_container'>
-        <h3>Upload a new photo</h3>
         <form method='post' action="http://d-c-a.000webhostapp.com/Fundraising/secondary/upload_fundraising.php" enctype="multipart/form-data">
             <input type=file name=upload_file accept="image/jpeg" style="display:none" onchange="this.parentElement.submit()">
             <input type=hidden name="fund_id" value="<?php echo $_GET['id'] ?>">
         </form>
-        <button id='edit_profile_btn' onclick="this.previousElementSibling.firstElementChild.click()"><i class="fa fa-camera" aria-hidden="true"></i>Upload</button>
+        <button id='upload_profile_btn' onclick="this.previousElementSibling.firstElementChild.click()"><i class="fa fa-camera" aria-hidden="true"></i>Upload new photo</button>
     </div>
 </div>
 
