0f570b4cccdaa168f82f4b27af9c37d9808812d1
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-08-18 20:33:44 +0530

    Add files via upload

--- confi/verify.php
+++ confi/verify.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     if(!isset($_SESSION['user_nic'])){
-        header("location:/anonymous");
+        header("location:/anonymous?location=".$_SERVER['REQUEST_URI']);
     }
