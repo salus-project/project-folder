d674c3fd11f3a5c7316d55728a3419dc52b7ac28
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-08-18 16:07:21 +0530

    Add files via upload

--- event/all_event.php
+++ event/all_event.php
@@ -1,67 +1,31 @@
 <?php
     require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
 ?>
-<link rel="stylesheet" href="/css_codes/organizations.css">
-<div id="org_body">
-    <form method='get' action='/organization'>
-        <div class='org_search'>
-            <div>Search for: </div><div class='search'><input placeholder='Event name' class='search_input' type=text></div>
-        </div>
-        <table id="table">
-            <?php
-                $event_detail=$con->query('select name,event_id,detail from disaster_events ORDER BY event_id DESC');
-
-                $result=$event_detail->fetch_all(MYSQLI_ASSOC);
-               // echo $event_detail;
-               // $events = array_unique(array_column($result,'LOWER(district)'));
-                
-                foreach($result as $row_dis){
-                    echo "<tr><td class='table_td org_list'>
-                    <div class='org_main'>
-                        <div class='org_logo_container'>";
-                            $org_profile_path =" http://d-c-a.000webhostapp.com/Organization/Profiles/" . $org[1] . ".jpg";
-                            echo '<img src="<?php echo $org_profile_path;?>" alt="Opps..." class="org_logo_pic">
-                        </div>
-                        <div class="org_name_des">
-                            <a href="/event/index.php?event_id='.$row_dis['event_id'].'"><button class="org_name" type="button" name="event_id" value="'.$row_dis['event_id'].'"><div class="name_only">'.ucfirst($row_dis['name']).'</div><div class="desc_only">'.$row_dis['detail'].'</div></button></a>
-                        </div>
-                        </td>
-                    </tr>';
-                   /* echo '<tr><td class="table_td"><div class="org_name_des">
-                        <a href="/event/index.php?event_id='.$row_dis['event_id'].'"><button class="org_name" type="button" name="event_id" value="'.$row_dis['event_id'].'">'.ucfirst($row_dis['name']).'<br>'.$row_dis['LOWER(affected_districts)'].'</button>
-                    </div></td></tr>';*/
-                }
-
-               /* function innerOrg($dis,$result){
-                    $org_array = array_filter(array_map(function($item) use($dis) {return org_filter($item,$dis);},$result));
-                    $orgs = array_map(function($item){return array(($item['org_name']),$item['org_id'],$item['discription']);},$org_array);
-                    foreach($orgs as $org){
-                        echo "<tr><td class='table_td org_list'>
-                            <div class='org_main'>
-                                <div class='org_logo_container'>";
-                                    $org_profile_path =" http://d-c-a.000webhostapp.com/Organization/Profiles/" . $org[1] . ".jpg";
-                                    echo '<img src="<?php echo $org_profile_path;?>" alt="Opps..." class="org_logo_pic">
-                                </div>
-                                <div class="org_name_des">
-                                    <button class="org_name" type="submit" name="selected_org" value="'.$org[1].'">'.ucfirst($org[0]).'<br>'.$org[2].'</button>
-                                </div>
-                                </td>
-                            </tr>';
-                    }
-                }
-                function org_filter($row,$org){
-                    if($row['LOWER(district)'] == $org){
-                        return $row;
-                    }
-                }
-                function countOrg($dis,$result){
-                    $org_array = array_filter(array_map(function($item) use($dis) {return org_filter($item,$dis);},$result));
-                    $orgs = array_map(function($item){return array(($item['org_name']),$item['org_id']);},$org_array);
-                    return count($orgs)+1;
-                }*/
-                 
-            ?>
-        </table>
-    </form>
+<link rel="stylesheet" href="/css_codes/all_event.css">
+<div id="event_body">   
+    <div class='event_search'>
+        <div>Search for: </div><div class='search'><input placeholder='Event name' class='search_input' type=text></div>
+    </div>
+    <table id="table">
+        <?php
+            $event_detail=$con->query('select name,event_id,detail from disaster_events ORDER BY event_id DESC');
+            $result=$event_detail->fetch_all(MYSQLI_ASSOC);
+            
+            foreach($result as $row_dis){
+                echo "<tr><td class='table_td event_list'>
+                <div class='event_main'>
+                    <div class='event_logo_container'>";
+                        $event_profile_path =" http://d-c-a.000webhostapp.com/Event/Profiles/" . $row_dis['event_id'] . ".jpg";
+                        echo '<img src="<?php echo $event_profile_path;?>" alt="Opps..." class="event_logo_pic">
+                    </div>
+                    <div class="event_name_des">
+                        <a href="/event/index.php?event_id='.$row_dis['event_id'].'"><button class="event_list_name" type="button" name="event_id" value="'.$row_dis['event_id'].'"><div class="name_only">'.ucfirst($row_dis['name']).'</div><div class="desc_only">'.$row_dis['detail'].'</div></button></a>
+                    </div>
+                </div>
+                </td>
+                </tr>';
+            }               
+        ?>
+    </table>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT']."/includes/footer.php" ?>
