368526bb48f79398aea16d52a5b07c685f52973f
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-08-18 18:18:23 +0530

    Add files via upload

--- organization/chat/chat_msg.php
+++ organization/chat/chat_msg.php
@@ -7,12 +7,12 @@
 	$dat=null;
 	
 	$org_id=$_GET["org_id"];
-	$sql="SELECT * FROM $org_id ORDER BY msg_id ASC";
+	$sql="SELECT * FROM $org_id ORDER BY msg_id DESC";
 	$result=$org_db->query($sql);
 	
 	date_default_timezone_set("Asia/Colombo");
 	$tdate= date("Y-m-d");
-	
+	$date_= date("Y-m-d");
 	if ($result->num_rows>0)
 	{
 		while ($row=$result->fetch_assoc())
@@ -20,19 +20,15 @@
 			$time=date("h:i:sa ", strtotime($row['time']));
 			$rowdate=date("Y M d ", strtotime($row['date']));
 			
-			if (($row['date']==$tdate) && ($dat !=$row['date'])){
-				echo "<div id='pdate'>Today</div>";
-				$dat=$row['date'];
-			}
-			elseif ($dat==null){
-				echo "<div id='pdate'>{$rowdate}</div>";
-				$dat=$row['date'];
-			}
-			else{
-				if($dat !=$row['date']){
-					echo "<div id='pdate'>{$rowdate}</div>";
-					$dat=$row['date'];
+
+			if ($date_ !=$row['date']){
+				if($date_==$tdate){
+					echo "<div id='pdate'>Today</div>";
 				}
+				else{
+					echo "<div id='pdate'>{$date_}</div>";
+				}
+				$date_ =$row['date'];
 			}
 					
 			if ($row['sender']==$_SESSION['first_name'] . ' ' . $_SESSION['last_name'])
--- organization/chat/index.php
+++ organization/chat/index.php
@@ -1,6 +1,15 @@
 <?php
-    require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
-	$org_id= "org_".$_GET['chat'];
+ob_start();
+ignore_user_abort();
+
+    require $_SERVER['DOCUMENT_ROOT'].'/organization/view_org_header.php';
+	$org_id= "org_".$_GET['org_id'];
+	if($text_role  == 'visitor'){
+		header("location:".(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] :"/organization/all_org.php"));
+		ob_end_flush();
+		ob_flush();
+		flush();
+	}
 ?>
 
 <!DOCTYPE html>
@@ -20,29 +29,31 @@
 		<div id="feedback">
 		</div>
 		
-		<form name=chat_form autocomplete="off">
-		<textarea rows="1" name="msg_b" id="msg_b" placeholder="type massage here"></textarea>   
+		
+		<input type="text" class="chat_input_tag" autocomplete="off" spellcheck="false" name="msg_b" id="msg_b" onkeypress="enterKey(event)" placeholder="Type massage here" />  
 		<input type="hidden" name="name" id="name" value="<?php echo  $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?>">
         <input type="hidden" name="id" id="id" value="<?php echo  $_SESSION['user_nic']; ?>"> 
 		<input type="hidden" name="org_id" id="org_id" value="<?php echo  $org_id; ?>"> 
         
 		<button id="post_btn" type="button">Send</button>
-		<span id="status">-</span>
 		
-		</form>
 		
+		</div>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 		<script>
             var is_success = true;
 			var position=0;
 
 			$(document).ready(function(){
-				$("#msg_b").keypress(function()
-				{
-					$("#status").html("Typing Message...");
-				});
+				
 			setInterval(loadchats,100);
 			$("#post_btn").click(function(){
+				post_msg();
+			});
+						
+			});
+
+			function post_msg(){
 				var name=$("#name").val();
 				var id=$("#id").val();
 				var message=$("#msg_b").val();
@@ -50,12 +61,16 @@
                 //console.log(name+' '+id+' '+message);
 				$.post("post.php",{name:name,message:message,id:id,org_id:org_id},function(data)
 				{
-					$("#status").html(data);
+					
 					$("#msg_b").val("");
 				});
-			});
-						
-			});
+			}
+
+			function enterKey(e){
+				if(e.keyCode==13){
+					post_msg();
+				}
+			}
 			
 			function loadchats()
 			{
@@ -94,6 +109,5 @@
 				
 			}
 		</script>
-	</body>
+	<?php require $_SERVER['DOCUMENT_ROOT']."/organization/org_footer.php"; ?>
 
-</html>
