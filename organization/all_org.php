85ab81b332b301aca0087c54f4cda9bf04174389
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-08-18 20:57:17 +0530

    Add files via upload

--- organization/all_org.php
+++ organization/all_org.php
@@ -1,6 +1,20 @@
 <?php
     require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
+    $query="select org_name,org_id,LOWER(district),discription from organizations ORDER BY district, org_name;
+    select organizations.org_name,organizations.org_id,role from org_members inner join organizations on org_members.org_id = organizations.org_id where org_members.NIC_num='".$_SESSION['user_nic']."' order by case when role = 'leader' then '1' when role = 'coleader' then '2' else role end asc;";
+    
+    if(mysqli_multi_query($con, $query)){
+        $sql_result = mysqli_store_result($con);
+        $result = mysqli_fetch_all($sql_result,MYSQLI_ASSOC);
+        mysqli_free_result($sql_result);
+    }
+    mysqli_next_result($con);
+    $sql_result = mysqli_store_result($con);
+    $my_org_detail = mysqli_fetch_all($sql_result,MYSQLI_ASSOC);
+    mysqli_free_result($sql_result);
+
 ?>
+
 <link rel="stylesheet" href="/css_codes/all_org.css">
 <div id="org_body">
     <form method='get' action='/organization'>
@@ -16,10 +30,32 @@
                 </div>
             </div>
         </div>
+        <div  class="org_div">
+            <div  class="org_role_div">
+                <table id="org_table">
+                    <tr>
+                        <th class="dis_name">My Organizations</th>
+                    </tr>
+                    <?php
+                    foreach($my_org_detail as $my_org){
+                        echo "<tr><td class='table_td org_list'>
+                            <div class='org_main'>
+                                <div class='org_logo_container'>";
+                                    $org_profile_path =" http://d-c-a.000webhostapp.com/Organization/Profiles/" . $my_org['org_id'] . ".jpg";
+                                    echo '<img src="<?php echo $org_profile_path;?>" alt="Opps..." class="org_logo_pic">
+                                </div>
+                                <div class="org_name_des">
+                                    <button class="org_name_btn" type="submit" name="selected_org" value="'.$my_org['org_id'].'">'.$my_org['org_name'].'<br>'.$my_org['role'].'</button>
+                                </div>
+                                </td>
+                            </tr>';
+                    }
+                    ?>
+                </table>
+            </div>
+        <div class="org_table_div">
         <table id="org_table">
             <?php
-                $org_detail=$con->query('select org_name,org_id,LOWER(district),discription from organizations ORDER BY district, org_name');
-                $result=$org_detail->fetch_all(MYSQLI_ASSOC);
                 
                 $districts = array_unique(array_column($result,'LOWER(district)'));
                 
@@ -58,6 +94,8 @@
                  
             ?>
         </table>
+            </div>
+            </div>
     </form>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT']."/includes/footer.php" ?>
