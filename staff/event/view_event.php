3f2b75eb5dfb6519f81623bc1d0909f3a2286976
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-08-20 10:56:28 +0530

    Add files via upload

--- staff/event/view_event.php
+++ staff/event/view_event.php
@@ -18,6 +18,8 @@
             $result=($con->query($query))->fetch_assoc();
             $event_status=$result['status'];
             $event_id=$result['event_id'];
+            $imgs = array_filter(explode(',', $result['img']));
+
         ?>
         
         <div id=event_header>
@@ -37,6 +39,7 @@
             ?>
             </div>
         </div>
+        <div class='event_body'> 
         <div class='detail_body'>
             <div class='table_container'>
                 <table class='event_table'>
@@ -55,6 +58,33 @@
                     ?>
                 </table>
             </div>
+        </div>
+        <div class="img_cont">
+            <div class='fund_head' colspan=2>Photos</div>
+            <!--div class='fund_image_conatainer'>
+                <div class='img_type'>Profile Image</div>
+                <div class="fund_image prim">
+                    <img src="http://d-c-a.000webhostapp.com/Event/<?php echo $id ?>.jpg" alt="Opps..." class="fund_pic">
+                </div>
+            </div-->
+            
+            <?php
+            foreach ($imgs as $img) {?>
+                <div class="fund_image_conatainer">
+                <div class='img_type'>Secondary Images</div>
+                    <div class="fund_image seco">
+                        <img src="http://d-c-a.000webhostapp.com/Event/<?php echo $img ?>.jpg" alt="Opps..." class="fund_pic">
+                    </div>
+                </div>
+            <?php }
+             echo "<div id=img_edit_btn_container >";
+                echo "<a href='/staff/event/event_edit_img?id=".$_GET['event_id']."'>";
+                echo "<button id='edit_img_btn' >Edit photos</button>";
+                echo "</a>";
+                echo"</div>";
+                
+            ?>
+        </div>
         </div>
     </body>
 </html>
