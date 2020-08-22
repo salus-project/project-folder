f6299a59100445cc7b883ce3207412334a544551
YathuNK <62744592+YathuNK@users.noreply.github.com>
2020-08-22 11:56:39 +0530

    Add files via upload

--- event/view_promises_on_me.php
+++ event/view_promises_on_me.php
@@ -2,6 +2,7 @@
     require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
 
     $id=$_SESSION['user_nic'];
+    $person=$_SESSION['first_name']." ".$_SESSION['last_name'];
     $event_id = $_GET['event_id'];
     $query="select * from event_".$event_id."_pro_don left join civilian_detail on civilian_detail.NIC_num = event_".$event_id."_pro_don.by_person left join organizations on organizations.org_id = event_".$event_id."_pro_don.by_org where to_person='".$id."';
     select a.id as id,a.don_id as don_id,a.item as item,a.amount as amount,a.pro_don as pro_don,b.to_person as to_person,b.note as note,b.by_org as by_org,b.by_person as by_person from event_".$event_id."_pro_don_content as a inner join event_".$event_id."_pro_don as b  on a.don_id = b.id where b.to_person='".$id."';";
@@ -62,12 +63,14 @@
                         $pending_array=[];
                         $full_array=[];
                         $id_arr=[];
+                        $pro_pend=[];
                         foreach($content_detail as $row_req1){
                             if ($row_req1['don_id']==$row_req['id']){
                                 $item_amount=$row_req1['item'].":".$row_req1['amount'];
                                 if (($row_req1['pro_don']=="promise") or ($row_req1['pro_don']=="pending")){
                                     array_push($promise_array,$item_amount);
                                     array_push($id_arr,$row_req1['id']);
+                                    array_push($pro_pend,$row_req1['pro_don']);
                                 }else if ($row_req1['pro_don']=="donated"){
                                     array_push($pending_array,$item_amount);
                                 }
@@ -86,12 +89,16 @@
                                 $note_data_row="<td rowspan=".count($promise_array).">".$note."</td>";}
                             else{
                                 $name_data_row=$note_data_row="";}
-                         
+                        if ($pro_pend[$x]=="promise"){
+                            $data_off="Not helped";
+                        } else{
+                            $data_off="Claimed";
+                        }
                         echo "  <tr onclick='edit_promise(\"/event/help/?event_id=".$event_id."&by=".$by."&to=".$id."\")'>
                             ".$name_data_row."
                             <td>".$value."</td>
                             <td class='not_click'>
-                            <div onclick='confirmFn(this,".$id_arr[$x].",".$event_id.")'><input type='checkbox' id='checkbox' disabled data-toggle='toggle' data-on='Helped' data-off='Not helped' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange='this.checked = !this.checked' ></div>
+                            <div onclick='confirmFn(this,".$id_arr[$x].",".$event_id.",\"".$name."\",\"".$by."\")'><input type='checkbox' id='checkbox' disabled data-toggle='toggle' data-on='Helped' data-off='".$data_off."' data-width='100' data-height='15' data-offstyle='warning' data-onstyle='success' onchange='this.checked = !this.checked' ></div>
                             </td>
                             ".$note_data_row."
                         </tr>";
@@ -165,7 +172,7 @@
                 
             }
         }
-        function confirmFn(ele,id,event) {
+        function confirmFn(ele,id,event,name,id_n) {
             swal({
                 title: "Are you sure?",
                 text: "Once confirmed, you will not be able to change this!",
@@ -175,14 +182,15 @@
             })
                 .then((willDelete) => {
                 if (willDelete) {
-                    toggleFn(event,id);
+                    toggleFn(event,id,name,id_n);
                     ele.outerHTML='<div class="toggle btn btn-success" data-toggle="toggle" disabled="disabled" style="width: 100px; height: 15px;"><input type="checkbox" id="checkbox" checked="" disabled="" data-toggle="toggle" data-on="Helped" data-off="Not helped" data-width="100" data-height="15" data-offstyle="warning" data-onstyle="success" onchange="this.checked = !this.checked"><div class="toggle-group"><label class="btn btn-success toggle-on" style="line-height: 20px;">Helped</label><label class="btn btn-warning active toggle-off" style="line-height: 20px;">Not helped</label><span class="toggle-handle btn btn-default"></span></div></div>';
                 } 
             });
         }
 
 
-        function toggleFn(event,id){
+        function toggleFn(event,id,name,id_n){
+        var person="<?php echo $person ; ?>";
         var sql="UPDATE `event_"+event+"_pro_don_content` SET `pro_don` = 'donated' WHERE `id` = "+id+"";;
         var xhttp = new XMLHttpRequest();
         // xhttp.onreadystatechange = function() {
@@ -192,7 +200,8 @@
         // };
         xhttp.open("POST", "/common/postajax/post_ajax.php", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send("sql="+sql);
+        xhttp.send("sql="+sql+"&person="+person+"&name="+name+"&event="+event+"&id_n="+id_n+"&type="+2);
+
     }
     </script>
 </html>
