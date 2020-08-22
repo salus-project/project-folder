909dbaeb842c4ac618f8e18c143c90a6fef7a9d6
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-08-22 12:45:57 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,85 @@
+<?php
+    require $_SERVER['DOCUMENT_ROOT']."/includes/header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/css_codes/about.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <div class="about-section">
+            <h1>About Us</h1>
+                <p style="text-align: justify;">Disaster and Crisis  Assistant (DCA) is the leading agency for disaster management in Sri Lanka. It is mandated with the responsibility of implementing and coordinating national and sub-national level programmes for reducing the risk of disasters with the participation of all relevant stakeholders.The main activities of the DCA are connect people in disaster situations and planning Preparedness, Emergency Response, Coordination of Relief and Post Disaster Activities in collaboration with other key agencies.In order to facilitate the coordination and implement all DMC activities, Disaster Management Committees were established at District, Divisional, Grama Niladhari Wasams, across the country. Also District Disaster Management Coordination Units (DDMCU) were established in all districts to carry out Disaster Risk Reduction (DRR) activities at the sub national level.
+                </p>
+                <h3>The principal functions of the DCA under the said Act are to</h3>
+                <p style="text-align: justify;">
+                    Assisting the Council in the preparation of the National Disaster Management Plan and the National Emergency Operation Plan and proposals for upgrading the same when it becomes necessary
+Taking responsibility for the implementation of the National Disaster Management Plan and the National Emergency Operation Plan, and upon the declaration of a state of disaster to direct and coordinate the implementation of the National Emergency Operation plan
+                </p>
+        </div>
+
+            <h2 style="text-align:center">Staff detail</h2>
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <div class="img_con">
+                            <img src="/staff/Profiles/login_img.png" alt="Avatar" class="avatar_1">
+                        </div>   
+                        <div class="container">
+                            <h3>Pranavan Jegatheeswaran</h3>
+                                <p class="title">CSE Undergraduate | UOM</p>
+                                <p>pranavan@gmail.com</p>
+                                <p><button class="button">Contact</button></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <div class="img_con">
+                            <img src="/staff/Profiles/login_img.png" alt="Avatar" class="avatar_1">
+                        </div>
+                        <div class="container">
+                            <h3>Yathursan Kalanantharasan</h3>
+                                <p class="title">CSE Undergraduate | UOM</p>
+                                <p>yathursan@gmail.com</p>
+                                <p><button class="button">Contact</button></p>
+                        </div>
+                    </div>
+                </div>
+  
+                <div class="column">
+                    <div class="card">
+                        <div class="img_con">
+                            <img src="/staff/Profiles/login_img1.png" alt="Avatar" class="avatar_1">
+                        </div>
+                        <div class="container">
+                            <h3>Saliny Vijayarasa</h3>
+                                <p class="title">CSE Undergraduate | UOM</p>
+                                <p>saliny@gmail.com</p>
+                                <p><button class="button">Contact</button></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <div class="img_con">
+                            <img src="/staff/Profiles/login_img1.png" alt="Avatar" class="avatar_1">
+                        </div>
+                        <div class="container">
+                            <h3>Thujitha Ponnuthurai</h3>
+                                <p class="title">CSE Undergraduate | UOM</p>
+                                <p>thujitha@gmail.com</p>
+                                <p><button class="button">Contact</button></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php include $_SERVER['DOCUMENT_ROOT']."/includes/footer.php" ?>
+    </body>
+</html>
+
