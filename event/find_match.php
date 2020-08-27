5d9032b46b2404d709ff29f5991b7467a3325ba9
saliny127 <62874404+saliny127@users.noreply.github.com>
2020-08-27 22:02:38 +0530

    Add files via upload

--- event/find_match.php
+++ event/find_match.php
@@ -1,17 +1,48 @@
 <style>
+.find_match_table_div{
+    width: 100%;
+    overflow: auto;
+}
 .find_match_table{
-    border: 1px solid black;
-    border-collapse:collapse;
+    border-collapse: collapse;
+    margin: 0;
 }
 
 .find_match_table th,.find_match_table td{
-    border: 1px solid black;
+    font-family: "Open Sans",sans-serif;
+    height: 37px;
+    padding: 5px;
+    margin: 0;
+    vertical-align: middle;
+    font-weight: 600;
+    
+    line-height: 1.8;
 }
-.find_match_table tr:hover{
-    background-color :#f5f5f5;
+.find_match_table th{
+    text-align: center;
+    font-size: 16px;
+}
+.find_match_table td{
+    text-align: left;
+    font-size: 14px;
 }
 
-
+.find_first_head{
+    background: #4c5fd7;
+    color: white;
+}
+.find_second_head{
+    background-color: #0c7dff47;
+}
+			
+tbody:hover,
+tr.hover,
+th.hover,
+td.hover,
+tr.hoverable:hover {
+    background-color: #f5f5f5;
+    cursor:pointer;
+}
 </style>
 
 
@@ -25,6 +56,7 @@ $type=$_GET['type'];
 ?>
     <div class='find_match_table_div'>
         <table class='find_match_table'>
+        
 <?php
 
     if ($type==1){
@@ -34,12 +66,12 @@ $type=$_GET['type'];
         $result2=$con->query($sql2)->fetch_all();
         $nic_arr=array_column($result2,0);
 
-        echo "<tr class='find_first_head'>";
-            echo "<th colspan=3>Volunteer Detail</th>";
+        echo "<tr>";
+            echo "<th class='find_first_head' colspan=3>Volunteer Detail</th>";
         echo "</tr>";
-        echo "<tr class='find_second_head'>";
-            echo "<th colspan=2>Person name</th>";
-            echo "<th colspan=1>Ablities</th>";
+        echo "<tr>";
+            echo "<th class='find_second_head' colspan=2>Person name</th>";
+            echo "<th class='find_second_head' colspan=1>Ablities</th>";
         echo "</tr>";
 
     }elseif($type==2){
@@ -60,15 +92,17 @@ $type=$_GET['type'];
     }
 
             $dis_arr=[];
+            echo "<tbody>";
             foreach($nic_arr as $key =>$n_id){
                 if(in_array($n_id,$dis_arr)){
                     echo "<tr><td >".$result2[$key][2]."</td></tr>";
                 }else{
                     array_push($dis_arr,$n_id);
-                    echo "<tr><td colspan=2 rowspan=".array_count_values($nic_arr)[$n_id].">".$result2[$key][1]."</td><td>".$result2[$key][2]."</td></tr>";
+                    echo "</tbody><tbody><tr><td colspan=2 rowspan=".array_count_values($nic_arr)[$n_id].">".$result2[$key][1]."</td><td>".$result2[$key][2]."</td></tr>";
                 }
+                
             }
+            echo "</tbody>";
         ?>
-
         </table>
     </div>
