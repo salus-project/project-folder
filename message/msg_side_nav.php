0ed317b1732a4efabf3b69b38cc98e957081db94
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-08-28 12:59:30 +0530

    Add files via upload

--- message/msg_side_nav.php
+++ message/msg_side_nav.php
@@ -13,14 +13,14 @@ for ($x = 0; $x < sizeof($result); $x++) {
     }
 //echo var_dump($detail);
 $nic_ar=array_map(function($ver){return "'".$ver."'";},$nic_arr);
-
-$sql="SELECT NIC_num,first_name,last_name,last_seen FROM civilian_detail WHERE NIC_num in (".implode(",",$nic_ar).");";
+$sql="SELECT NIC_num,first_name,last_name,last_seen FROM civilian_detail WHERE NIC_num in (".(implode(",",$nic_ar)?:"''").");";
 $result=array_reverse($con->query($sql)->fetch_all(MYSQLI_ASSOC));
 //print_r ($result);
 $result_nic=array_column($result,"NIC_num");
 echo "<div class='Msg_page_container' >";
 echo "<div class='Msg_nav_container' >";
-echo "<div id='Msg_search_container' class='Msg_search_container' >";
+echo "<div id='Msg_search_container' class='Msg_search_container' >
+        <input id='message_inp' type='text' placeholder='Search here'>";
 echo "</div>";
 echo "<div class='msg_nav_s_cont'>";
 for ($x = 0; $x < sizeof($nic_arr); $x++) {
@@ -62,5 +62,5 @@ echo "</div>";
 
 ?> 
 <script>
-        autocomplete_ready(document.getElementById("Msg_search_container"), 'users', 'ready', '/message/?to_person=');
+        autocomplete_ready(document.getElementById("message_inp"), 'users', 'ready', '/message/?to_person=');
 </script>
