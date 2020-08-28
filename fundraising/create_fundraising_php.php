60be210b9f44a580a18436298a8906e46a0605bc
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-08-28 15:53:51 +0530

    Add files via upload

--- fundraising/create_fundraising_php.php
+++ fundraising/create_fundraising_php.php
@@ -53,16 +53,10 @@
     }
 
     if($isOk==1){
-        $query="INSERT INTO public_posts (`author`, `org`, `date`) VALUES ('$by',$org_id,NOW())";
-        $con->query($query);
-        $last_id=$con->insert_id;
 
-        $query1="INSERT INTO `fundraisings` (`name`, `by_civilian`, `by_org`, `for_event`, `for_any`, `service_area`, `description`,`post_id`) VALUES ('$fundraising_name', '$by',$org_id, $for_event, '$for_any', '$district','$description',$last_id)";
-        $query_run1=mysqli_query($con,$query1);
-        $last_id2=$con->insert_id;
+        $query1="INSERT INTO `fundraisings` (`name`, `by_civilian`, `by_org`, `for_event`, `for_any`, `service_area`, `description`) VALUES ('$fundraising_name', '$by',$org_id, $for_event, '$for_any', '$district','$description');";
 
         $querry_arr=array();
-        $query2="UPDATE public_posts set fund='$last_id2' WHERE post_index=".$last_id.";";
         for($x=0 ; $x < count($item) ; $x++){
             if(!empty($item[$x])){
                 $item1=filt_inp(ready_input($item[$x]));
@@ -71,14 +65,15 @@
                         $amount1=0; 
                         
                 }
-                array_push($querry_arr, "('$last_id2','".$item1."','$amount1')");
+                array_push($querry_arr, "('(select last_insert_id())','".$item1."','$amount1')");
             }
         }
-        $query2.= "INSERT INTO `fundraisings_expects`(`fund_id`, `item`, `amount`) VALUES ". implode(",", $querry_arr).";";
+        $query1.= "INSERT INTO `fundraisings_expects`(`fund_id`, `item`, `amount`) VALUES ". implode(",", $querry_arr).";";
         
-        $query_run2=mysqli_multi_query($con,$query2);
+        $query_run=mysqli_multi_query($con,$query1);
 
-        if($query_run1 && $query_run2){
+
+        if($query_run){
 
             echo '<script type="text/javascript">alert("Successfully created")</script>';
             header("Location:/fundraising");
