eab28bb630ffd8e4b96bd982fe888e2414f85c88
Thujitha Ponnuthurai <59986130+Thuji98@users.noreply.github.com>
2020-09-26 18:50:13 +0530

    Add files via upload

--- staff/create_civilian_acc.php
+++ staff/create_civilian_acc.php
@@ -8,11 +8,12 @@
 
 <link rel="stylesheet" href="/staff/css_codes/create_civilian_acc.css">
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
 <script>
     btnPress(2);
 </script>
-
+ 
     <form method='post' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>' ng-app="" name="CreateCivilianAccForm" id='CreateCivilianAccForm' novalidate ng-init="Nic='<?php echo $nic?>';firstName='<?php echo $first_name?>';lastName='<?php echo $last_name?>';userPhone='<?php echo $phone_number?>';userMail='<?php echo $email_address?>';">
         <div class="create_civ_form_box">
 
@@ -26,7 +27,8 @@
                 <label class="create_civ_form_label">NIC number</label>
                 <div>
                     <div ng-class="{'has-error': CreateCivilianAccForm.nic.$invalid}">
-                        <input class="create_civ_input_box" type='text' name='nic' placeholder='Enter NIC number' value='' pattern="[0-9]{9}V|[0-9]{9}v|[0-9]{11}" ng-model="Nic" required>
+                        <input class="create_civ_input_box" type='text' name='nic' placeholder='Enter NIC number' max_length="11"  pattern="[0-9]{9}[Vv]|[0-9]{11}" ng-model="Nic" required>
+                        <span class='error'><?php echo $nic_err ?></span>
                     </div>
                     <span class='error' data-ng-show="CreateCivilianAccForm.nic.$invalid && CreateCivilianAccForm.nic.$touched"><i class='fas fa-exclamation-circle'></i> Invalid NIC Format</span>
                 </div>
@@ -37,6 +39,7 @@
                 <div>
                     <div ng-class="{'has-error': (CreateCivilianAccForm.first_name.$invalid && CreateCivilianAccForm.first_name.$touched)}">
                         <input name = "first_name" type="text" class="create_civ_input_box" placeholder="Enter first name" ng-model="firstName" required>
+                        <span class='error'><?php echo $first_name_err ?></span>
                     </div>
                     <span class='error' data-ng-show="CreateCivilianAccForm.first_name.$invalid && CreateCivilianAccForm.first_name.$touched"><i class='fas fa-exclamation-circle'></i> First name Required</span>
                 </div>
@@ -47,6 +50,7 @@
                 <div>
                     <div ng-class="{'has-error': (CreateCivilianAccForm.last_name.$invalid && CreateCivilianAccForm.last_name.$touched)}">
                         <input name = "last_name" type="text" class="create_civ_input_box" placeholder="Enter last name" ng-model="lastName" required>
+                        <span class='error'><?php echo $last_name_err ?></span>
                     </div>
                     <span class='error' data-ng-show="CreateCivilianAccForm.last_name.$invalid && CreateCivilianAccForm.last_name.$touched"><i class='fas fa-exclamation-circle'></i> Last name Required</span>
                 </div>
@@ -56,7 +60,8 @@
                 <label class="create_civ_form_label">Email </label>
                 <div>
                     <div ng-class="{'has-error': CreateCivilianAccForm.email_address.$invalid}">
-                        <input name = "email_address" type="email" class="create_civ_input_box" placeholder="Enter email address" ng-model="userMail" required/>
+                        <input name = "email_address" type="email" class="create_civ_input_box" placeholder="Enter email address" ng-model="userMail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/>
+                        <span class='error'><?php echo $email_address_err ?></span>
                     </div>
                     <span class='error' data-ng-show="CreateCivilianAccForm.email_address.$error.email && CreateCivilianAccForm.email_address.$touched"><i class='fas fa-exclamation-circle'></i> Invalid Email</span>
                 </div>
@@ -134,13 +139,14 @@
                 <div>
                     <div ng-class="{'has-error': (CreateCivilianAccForm.phone_number.$invalid && CreateCivilianAccForm.phone_number.$touched)}">
                         <input name = "phone_number" type="tel" class="create_civ_input_box" placeholder="Enter phone number" pattern="[0-9]{9}|[0-9]{10}" ng-model="userPhone"/>
+                        <span class='error'><?php echo $phone_number_err ?></span>
                     </div>
                     <span class='error' data-ng-show="CreateCivilianAccForm.phone_number.$invalid && CreateCivilianAccForm.phone_number.$touched"><i class='fas fa-exclamation-circle'></i> Invalid Number Format</span>
                 </div>
             </div>
             </div>
             <div class="foot_create_civ_form">
-                <button name="submit" type="submit"  value="Update"  class="create_civ_submit_button submitt">Submit</button>
+                <button id="submitBtn" class="create_civ_submit_button submitt">Submit</button>
                 <a href="<?php echo $_SERVER['HTTP_REFERER']?>"><button name="cancel_button" type="button"  value="Cancel"  class="create_civ_submit_button">Cancel</button></a>
             </div>
             
@@ -157,7 +163,13 @@
         $scope.data = {};
 
     }]);
-
+    document.getElementById('submitBtn').onclick = function(e){
+        if(($('input[name$="nic"]:first').hasClass('ng-invalid') || $('input[name$="first_name"]:first').hasClass('ng-invalid') || $('input[name$="last_name"]:first').hasClass('ng-invalid') || $('input[name$="email_address"]:first').hasClass('ng-invalid')|| $('input[name$="phone_number"]:first').hasClass('ng-invalid') )){
+            alert('Please Fill the form Correctly');
+        }else{
+            document.getElementById('CreateCivilianAccForm').submit();
+        }
+    }
     //         Custom select           //
 
     var x, i, j, l, ll, selElmnt, a, b, c;
--- staff/create_civilian_acc_php.php
+++ staff/create_civilian_acc_php.php
@@ -1,76 +1,117 @@
 <?php
-if (isset($_POST['submit'])){
-    $nic = strtoupper(rtrim($_POST["nic"]));
-    $first_name = $_POST["first_name"];
-    $last_name = $_POST["last_name"];
-    $email_address = $_POST["email_address"];
-    $gender = ($_POST["gender"]!='')?"'".$_POST["gender"]."'":"NULL";
-    $address = ($_POST["address"]!='')?"'".$_POST["address"]."'":"NULL";
-    $district = ($_POST["district"]!='')?"'".$_POST["district"]."'":"NULL";
-    $village = ($_POST["village"]!='')?"'".$_POST["village"]."'":"NULL";
-    $street = ($_POST["street"]!='')?"'".$_POST["street"]."'":"NULL";
-    $occupation = ($_POST["occupation"]!='')?"'".$_POST["occupation"]."'":"NULL";
-    $phone_number = ($_POST["phone_number"]!='')?"'".$_POST["phone_number"]."'":"NULL";
 
+if (isset($_POST['nic'])){
+    echo "Please try again";
+    $isOk=1;
+    $nic = strtoupper(filt_inp($_POST["nic"]));
+    if (!preg_match("/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/",$nic)){
+        $isOk=0;
+        $nic_err="Invalid NIC number";
+    }
+
+    $first_name = ucfirst(filt_inp($_POST["first_name"]));
+    if(!preg_match("/^[a-zA-Z]*$/", $first_name)){
+        $isOk=0;
+        $first_name_err="Characters only";
+    }
+
+    $last_name = ucfirst(filt_inp($_POST["last_name"]));
+    if(!preg_match("/^[a-zA-Z]*$/", $last_name)){
+        $isOk=0;
+        $last_name_err="Characters only";
+    }
+
+    $email_address = filt_inp($_POST["email_address"]);
+    if (!filter_var($email_address, FILTER_VALIDATE_EMAIL) && $email_address!='') {
+        $email_address_err = "Invalid email format";
+        $isOk=0;
+    }
+
+    $gender = filt_inp($_POST["gender"]); 
+    $address = filt_inp($_POST["address"]);
+    $district = filt_inp($_POST["district"]);
+    $village = filt_inp($_POST["village"]);
+    $street = filt_inp($_POST["street"]);
+    $occupation = filt_inp($_POST["occupation"]);
+    $phone_number = filt_inp($_POST["phone_number"]);
+
+    if(!preg_match("/^[0-9]{10}$/",$phone_number) and $_POST["phone_number"]!=''){
+        $phone_number_err='Only 10 numbers allowed';
+        $isOk=0;
+    }
 
     $nic_check='SELECT NIC_num FROM `civilian_detail` WHERE NIC_num="'.$nic.'"';
     $result_=$con->query($nic_check);
     if (mysqli_num_rows($result_) > 0){
         echo '<script type="text/javascript"> alert ("This NIC number already have an account") </script>';
-    }else{
-        if ($email_address != ""){
-            
-            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            
-            function generate_string($input, $strength = 10) {
-                $input_length = strlen($input);
-                $random_string = '';
-                for($i = 0; $i < $strength; $i++) {
-                    $random_character = $input[mt_rand(0, $input_length - 1)];
-                    $random_string .= $random_character;
-                }
-                return $random_string;
+        $isOk=0;
+    }
+    if ($email_address != ""){
+        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        function generate_string($input, $strength = 10) {
+            $input_length = strlen($input);
+            $random_string = '';
+            for($i = 0; $i < $strength; $i++) {
+                $random_character = $input[mt_rand(0, $input_length - 1)];
+                $random_string .= $random_character;
             }
-
-            $password= generate_string($permitted_chars) ;
-
-            $sql = "INSERT INTO civilian_detail (password, first_name, last_name, gender, NIC_num, address, district,village,street, Occupation, phone_num, email)
-            VALUES ('$password', '$first_name', '$last_name', $gender, '$nic', $address, $district, $village,$street,$occupation, $phone_number, '$email_address')";
-            $query_run= mysqli_query($con,$sql);
-
-            echo $sql;
-
-            if($query_run ){
-                
-                $query="ALTER TABLE disaster_events ADD COLUMN `user_".$nic."` varchar(50) NOT NULL DEFAULT 'not_set not_requested not_applied'";
-                $query_run= mysqli_query($con,$query);
-
-                $query1="";
-                $query1.= "create table `user_notif_ic_".$nic."` (Notification_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY not null,Date date, Time time,Content text, link varchar(100), status varchar(10) default 'unseen');"; 
-                $query1.="create table `user_message_".$nic."` (id int(5) AUTO_INCREMENT primary key, _from varchar(12) default null, _to varchar(12) default null, time datetime, content text, status tinyint(1) default 0);";
-                $notification_DB = NotificationDb::getConnection();
-                $query1_run= mysqli_multi_query($notification_DB,$query1);
-
-                $to_email = $email_address ;
-                $subject = 'Your DCA account was created successfully';
-                $message = 'Your DCA account was just created successfully and your password is '.$password.', change your password by log-in to your DCA account.';
-                $headers = 'From: kanthankanthan111@gmail.com';
-                set_error_handler(function() { /* ignore errors */ });
-                    if(mail($to_email,$subject,$message,$headers)){
-                    }
-                restore_error_handler();
-
-                copy($_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/default.jpg', $_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/'.$nic.'.jpg');
-                copy($_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/resized/default.jpg', $_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/resized/'.$nic.'.jpg');
-                copy($_SERVER['DOCUMENT_ROOT'] . '/common/documents/Covers/default.jpg', $_SERVER['DOCUMENT_ROOT'] . '/common/documents/Covers/'.$nic.'.jpg');
-
-                header('location:member.php');
-            }else{
-                echo '<script type="text/javascript"> alert ("Data not Uploaded") </script>';
-            }
-        }else{
-            echo '<script type="text/javascript"> alert ("Enter email") </script>';
+            return $random_string;
         }
+
+        $password= generate_string($permitted_chars) ;
+
+    }else{
+        echo '<script type="text/javascript"> alert ("Enter email") </script>';
+    }
+
+    if($isOk==1){
+        $gender = ($_POST["gender"]!='')?"'".filt_inp($_POST["gender"])."'":"NULL"; 
+        $address = ($_POST["address"]!='')?"'".filt_inp($_POST["address"])."'":"NULL";
+        $district = ($_POST["district"]!='')?"'".filt_inp($_POST["district"])."'":"NULL";
+        $village = ($_POST["village"]!='')?"'".filt_inp($_POST["village"])."'":"NULL";
+        $street = ($_POST["street"]!='')?"'".filt_inp($_POST["street"])."'":"NULL";
+        $occupation = ($_POST["occupation"]!='')?"'".filt_inp($_POST["occupation"])."'":"NULL";
+        $phone_number = ($_POST["phone_number"]!='')?"'".filt_inp($_POST["phone_number"])."'":"NULL";
+
+        
+        
+        $sql = "INSERT INTO civilian_detail (password, first_name, last_name, gender, NIC_num, address, district,village,street, Occupation, phone_num, email)
+        VALUES ('$password', '$first_name', '$last_name', $gender, '$nic', $address, $district, $village,$street,$occupation, $phone_number, '$email_address')";
+        $query_run= mysqli_query($con,$sql);
+
+        echo $sql;
+
+        if($query_run ){
+            
+            $query="ALTER TABLE disaster_events ADD COLUMN `user_".$nic."` varchar(50) NOT NULL DEFAULT 'not_set not_requested not_applied'";
+            $query_run= mysqli_query($con,$query);
+
+            $query1="";
+            $query1.= "create table `user_notif_ic_".$nic."` (Notification_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY not null,Date date, Time time,Content text, link varchar(100), status varchar(10) default 'unseen');"; 
+            $query1.="create table `user_message_".$nic."` (id int(5) AUTO_INCREMENT primary key, _from varchar(12) default null, _to varchar(12) default null, time datetime, content text, status tinyint(1) default 0);";
+            $notification_DB = NotificationDb::getConnection();
+            $query1_run= mysqli_multi_query($notification_DB,$query1);
+
+            $to_email = $email_address ;
+            $subject = 'Your DCA account was created successfully';
+            $message = 'Your DCA account was just created successfully and your password is '.$password.', change your password by log-in to your DCA account.';
+            $headers = 'From: kanthankanthan111@gmail.com';
+            set_error_handler(function() { /* ignore errors */ });
+                if(mail($to_email,$subject,$message,$headers)){
+                }
+            restore_error_handler();
+
+            copy($_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/default.jpg', $_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/'.$nic.'.jpg');
+            copy($_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/resized/default.jpg', $_SERVER['DOCUMENT_ROOT'] . '/common/documents/Profiles/resized/'.$nic.'.jpg');
+            copy($_SERVER['DOCUMENT_ROOT'] . '/common/documents/Covers/default.jpg', $_SERVER['DOCUMENT_ROOT'] . '/common/documents/Covers/'.$nic.'.jpg');
+
+            header('location:member.php');
+        }else{
+            echo '<script type="text/javascript"> alert ("Data not Uploaded") </script>';
+        }
+        
+    }else{
+        echo '<script type="text/javascript"> alert ("Please fill form correctly") </script>';
     }
 }
 ?>
