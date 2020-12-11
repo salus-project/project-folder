586a40f0c3ebe7858b37951a5c7422180db8c1c7
jegatheesprana <62617477+jegatheesprana@users.noreply.github.com>
2020-12-11 14:08:40 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
             <?php include $_SERVER['DOCUMENT_ROOT']."/includes/side_nav.php" ?>
         </div>
         <div class="main_footer <?php echo (($_SESSION['side_nav']=='1')?'':'full_footer'); ?>" id="main_footer">
-            © 2020 Copyright: Salus Team test
+            © 2020 Copyright: Salus Team test2
         </div>
     </body>
 </html>
